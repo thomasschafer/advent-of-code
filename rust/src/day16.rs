40a1b03401dd768a8ac2b751cbf0be490fa8b560
thomasschafer <thomasschafer97@gmail.com>
2022-12-23 17:37:15 +0000

    add day 16 part 1 solution

---
+++ rust/src/day16.rs
@@ -0,0 +1,118 @@
+use std::collections::{HashMap, HashSet};
+use std::time::SystemTime;
+use std::{cmp, fs};
+
+#[derive(Debug)]
+struct Valve {
+    flow_rate: u32,
+    leads_to: Vec<String>,
+}
+
+fn parse_valve(row: &str) -> (&str, Valve) {
+    let valve_name = row.split(" ").collect::<Vec<&str>>()[1];
+    let flow_rate: u32 = row.split("flow rate=").collect::<Vec<&str>>()[1]
+        .split(";")
+        .collect::<Vec<&str>>()[0]
+        .parse()
+        .unwrap();
+    let leads_to = row.split("to v").collect::<Vec<&str>>()[1]
+        .split(" ")
+        .collect::<Vec<&str>>()[1..]
+        .join("")
+        .split(",")
+        .map(|c| c.to_string())
+        .collect::<Vec<String>>();
+    let valve = Valve {
+        flow_rate,
+        leads_to,
+    };
+    (valve_name, valve)
+}
+
+fn parse_valves(rows: Vec<&str>) -> HashMap<&str, Valve> {
+    let mut valves: HashMap<&str, Valve> = HashMap::new();
+    for row in rows {
+        let (valve_name, valve) = parse_valve(row);
+        valves.insert(valve_name, valve);
+    }
+    valves
+}
+
+fn cache_key(mins_remaining: u32, current_valve: &str, open_valves: &HashSet<&str>) -> String {
+    let mut valves_vec = Vec::from_iter(open_valves.clone());
+    valves_vec.sort();
+    let res = [
+        mins_remaining.to_string(),
+        current_valve.to_string(),
+        valves_vec.join("-").to_string(),
+    ]
+    .join(",");
+    res
+}
+
+fn most_pressure_released_recursive(
+    valves: &HashMap<&str, Valve>,
+    mins_remaining: u32,
+    current_valve: &str,
+    open_valves: &HashSet<&str>,
+    result_cache: &mut HashMap<String, u32>,
+) -> u32 {
+    if mins_remaining == 0 {
+        return 0;
+    }
+    let current_cache_key = cache_key(mins_remaining, current_valve, open_valves);
+    if let Some(result) = result_cache.get(&current_cache_key) {
+        return *result;
+    }
+    let mut max_result = 0;
+
+    if !open_valves.contains(current_valve) && valves.get(current_valve).unwrap().flow_rate > 0 {
+        let mut new_open_valves = open_valves.clone();
+        new_open_valves.insert(current_valve);
+        let open_valve_result = valves.get(current_valve).unwrap().flow_rate * (mins_remaining - 1)
+            + most_pressure_released_recursive(
+                &valves,
+                mins_remaining - 1,
+                current_valve,
+                &new_open_valves,
+                result_cache,
+            );
+        max_result = cmp::max(max_result, open_valve_result)
+    }
+
+    for next_valve in &valves.get(current_valve).unwrap().leads_to {
+        let next_valve_result = most_pressure_released_recursive(
+            &valves,
+            mins_remaining - 1,
+            next_valve,
+            &open_valves,
+            result_cache,
+        );
+        max_result = cmp::max(max_result, next_valve_result);
+    }
+    result_cache.insert(current_cache_key, max_result);
+    max_result
+}
+
+fn most_pressure_released(valves: &HashMap<&str, Valve>, mins: u32, current_valve: &str) -> u32 {
+    most_pressure_released_recursive(
+        &valves,
+        mins,
+        current_valve,
+        &HashSet::new(),
+        &mut HashMap::new(),
+    )
+}
+
+pub fn part1(file_path: &str, mins_remaining: u32) -> u32 {
+    let file_contents = fs::read_to_string(file_path).unwrap();
+    let rows = file_contents.split("\n").collect::<Vec<&str>>();
+    let valves = parse_valves(rows);
+    let start_time = SystemTime::now();
+    let result = most_pressure_released(&valves, mins_remaining, "AA");
+    println!(
+        "Time taken: {}",
+        (start_time.elapsed().unwrap().as_millis() as f32) / 1000.0
+    );
+    result
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,8 @@
-mod day15;
+mod day16;
 
 fn main() {
-    println!("Part 1 result (test data): {}", day15::part1("../data/day_15_test.txt", 10));
-    println!("Part 1 result (real data): {}", day15::part1("../data/day_15.txt", 2000000));
-    println!("Part 2 result (test data): {}", day15::part2("../data/day_15_test.txt", 0, 20));
-    println!("Part 2 result (real data): {}", day15::part2("../data/day_15.txt", 0, 4000000));
+    println!("Part 1 result (test data): {}", day16::part1("../data/day_16_test.txt", 30));
+    println!("Part 1 result (real data): {}", day16::part1("../data/day_16.txt", 30));
+    // println!("Part 2 result (test data): {}", day16::part2("../data/day_16_test.txt"));
+    // println!("Part 2 result (real data): {}", day16::part2("../data/day_16.txt"));
 }
