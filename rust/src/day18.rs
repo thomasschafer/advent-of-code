c54d1006143da0451ca0c07916f864eede7cb784
thomasschafer <thomasschafer97@gmail.com>
2022-12-29 21:16:04 +0000

    add day 18 part 2 solution

--- rust/src/day18.rs
+++ rust/src/day18.rs
@@ -1,10 +1,32 @@
-use std::{collections::HashMap, fs};
+use f64;
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Position {
     x: i32,
     y: i32,
     z: i32,
+}
+
+fn parse_positions(file_path: &str) -> Vec<Position> {
+    let file_contents = fs::read_to_string(file_path).unwrap();
+    let positions = file_contents
+        .split("\n")
+        .map(|row| {
+            row.split(",")
+                .map(|val| val.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .map(|position| Position {
+            x: position[0],
+            y: position[1],
+            z: position[2],
+        })
+        .collect::<Vec<Position>>();
+    positions
 }
 
 fn next_positions(position: &Position) -> [Position; 6] {
@@ -49,7 +71,6 @@ fn surface_area_recursive(
     positions_map.insert(*current_position, true);
     let mut surface_area_result = 0;
     for next_pos in next_positions(current_position) {
-        // println!("--- next_pos = {:?}", next_pos);
         if let Some(visited) = positions_map.get(&next_pos) {
             if !*visited {
                 surface_area_result += surface_area_recursive(&next_pos, positions_map);
@@ -75,25 +96,107 @@ fn surface_area(positions: Vec<Position>) -> u64 {
     total_surface_area
 }
 
-pub fn part1(file_path: &str) -> u64 {
-    let file_contents = fs::read_to_string(file_path).unwrap();
-    let positions = file_contents
-        .split("\n")
-        .map(|row| {
-            row.split(",")
-                .map(|val| val.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>()
-        })
-        .map(|position| Position {
-            x: position[0],
-            y: position[1],
-            z: position[2],
-        })
-        .collect::<Vec<Position>>();
+#[derive(Debug)]
+struct CubeBoundary {
+    min_x: f64,
+    max_x: f64,
+    min_y: f64,
+    max_y: f64,
+    min_z: f64,
+    max_z: f64,
+}
 
+impl CubeBoundary {
+    fn new() -> CubeBoundary {
+        CubeBoundary {
+            min_x: f64::INFINITY,
+            max_x: f64::NEG_INFINITY,
+            min_y: f64::INFINITY,
+            max_y: f64::NEG_INFINITY,
+            min_z: f64::INFINITY,
+            max_z: f64::NEG_INFINITY,
+        }
+    }
+
+    fn update(&mut self, position: Position) {
+        if ((position.x - 1) as f64) < self.min_x {
+            self.min_x = (position.x - 1) as f64;
+        }
+        if (position.x + 1) as f64 > self.max_x {
+            self.max_x = (position.x + 1) as f64;
+        }
+        if ((position.y - 1) as f64) < self.min_y {
+            self.min_y = (position.y - 1) as f64;
+        }
+        if (position.y + 1) as f64 > self.max_y {
+            self.max_y = (position.y + 1) as f64;
+        }
+        if ((position.z - 1) as f64) < self.min_z {
+            self.min_z = (position.z - 1) as f64;
+        }
+        if (position.z + 1) as f64 > self.max_z {
+            self.max_z = (position.z + 1) as f64;
+        }
+    }
+
+    fn contains(&self, position: Position) -> bool {
+        self.min_x <= position.x as f64
+            && self.max_x >= position.x as f64
+            && self.min_y <= position.y as f64
+            && self.max_y >= position.y as f64
+            && self.min_z <= position.z as f64
+            && self.max_z >= position.z as f64
+    }
+}
+
+fn external_surface_area_recursive(
+    current_position: Position,
+    droplet_positions: &HashSet<Position>,
+    visited: &mut HashSet<Position>,
+    boundary: &CubeBoundary,
+) -> u64 {
+    visited.insert(current_position);
+    let mut surface_area_result = 0;
+    for next_pos in next_positions(&current_position) {
+        if visited.contains(&next_pos) || !boundary.contains(next_pos) {
+            continue;
+        }
+        if droplet_positions.contains(&next_pos) {
+            surface_area_result += 1;
+        } else {
+            surface_area_result +=
+                external_surface_area_recursive(next_pos, droplet_positions, visited, boundary);
+        }
+    }
+    surface_area_result
+}
+
+fn external_surface_area(positions: Vec<Position>) -> u64 {
+    let mut droplet_positions: HashSet<Position> = HashSet::new();
+    let mut boundary = CubeBoundary::new();
+    for position in &positions {
+        droplet_positions.insert(*position);
+        boundary.update(*position);
+    }
+    let mut visited: HashSet<Position> = HashSet::new();
+    external_surface_area_recursive(
+        Position {
+            x: boundary.min_x as i32,
+            y: boundary.min_y as i32,
+            z: boundary.min_z as i32,
+        },
+        &droplet_positions,
+        &mut visited,
+        &boundary,
+    )
+}
+
+pub fn part1(file_path: &str) -> u64 {
+    let positions = parse_positions(file_path);
     surface_area(positions)
 }
 
 pub fn part2(file_path: &str) -> u64 {
-    1
+    let positions = parse_positions(file_path);
+    external_surface_area(positions)
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,6 +3,6 @@ mod day18;
 fn main() {
     println!("Part 1 result (test data): {}", day18::part1("../data/day_18_test.txt"));
     println!("Part 1 result (real data): {}", day18::part1("../data/day_18.txt"));
-    // println!("Part 2 result (test data): {}", day18::part2("../data/day_18_test.txt"));
-    // println!("Part 2 result (real data): {}", day18::part2("../data/day_18.txt"));
+    println!("Part 2 result (test data): {}", day18::part2("../data/day_18_test.txt"));
+    println!("Part 2 result (real data): {}", day18::part2("../data/day_18.txt"));
 }
