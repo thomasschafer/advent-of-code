d6a6ef4b0f11de1cb0157ff8dbf1f29e78bcf50d
thomasschafer <thomasschafer97@gmail.com>
2023-01-02 22:17:16 +0000

    add remainder of day 19 part 1 solution

--- rust/src/day19.rs
+++ rust/src/day19.rs
@@ -1,11 +1,11 @@
 use regex::{Captures, Regex};
-use std::{fmt, fs, str::FromStr};
+use std::{cmp, collections::HashMap, fmt, fs, str::FromStr, time::SystemTime};
 
 #[derive(Debug)]
 struct Cost {
-    ore: u32,
-    clay: u32,
-    obsidian: u32,
+    ore: u64,
+    clay: u64,
+    obsidian: u64,
 }
 
 #[derive(Debug)]
@@ -35,24 +35,24 @@ fn parse_blueprint_from_line(line: &str) -> Blueprint {
     Blueprint {
         id: capture_get::<u64>(&capture, "blueprint_id"),
         ore_robot_cost: Cost {
-            ore: capture_get::<u32>(&capture, "ore_robot_cost_ore"),
+            ore: capture_get::<u64>(&capture, "ore_robot_cost_ore"),
             clay: 0,
             obsidian: 0,
         },
         clay_robot_cost: Cost {
-            ore: capture_get::<u32>(&capture, "clay_robot_cost_ore"),
+            ore: capture_get::<u64>(&capture, "clay_robot_cost_ore"),
             clay: 0,
             obsidian: 0,
         },
         obsidian_robot_cost: Cost {
-            ore: capture_get::<u32>(&capture, "obsidian_robot_cost_ore"),
-            clay: capture_get::<u32>(&capture, "obsidian_robot_cost_clay"),
+            ore: capture_get::<u64>(&capture, "obsidian_robot_cost_ore"),
+            clay: capture_get::<u64>(&capture, "obsidian_robot_cost_clay"),
             obsidian: 0,
         },
         geode_robot_cost: Cost {
-            ore: capture_get::<u32>(&capture, "geode_robot_cost_ore"),
+            ore: capture_get::<u64>(&capture, "geode_robot_cost_ore"),
             clay: 0,
-            obsidian: capture_get::<u32>(&capture, "geode_robot_cost_obsidian"),
+            obsidian: capture_get::<u64>(&capture, "geode_robot_cost_obsidian"),
         },
     }
 }
@@ -70,18 +70,190 @@ fn parse_blueprints(file_path: &str) -> Vec<Blueprint> {
         .collect::<Vec<Blueprint>>()
 }
 
-fn largest_num_geodes_opened(blueprint: &Blueprint, mins: u32) -> u64 {
-    1
+#[derive(Debug, Eq, Hash, PartialEq)]
+struct MiningAssets {
+    mins: u64,
+    ore: u64,
+    ore_robots: u64,
+    clay: u64,
+    clay_robots: u64,
+    obsidian: u64,
+    obsidian_robots: u64,
 }
 
-fn sum_of_quality_levels_of_blueprints(blueprints: Vec<Blueprint>, mins: u32) -> u64 {
+fn largest_num_geodes_opened_recursive(
+    blueprint: &Blueprint,
+    mins: u64,
+    ore: u64,
+    ore_robots: u64,
+    clay: u64,
+    clay_robots: u64,
+    obsidian: u64,
+    obsidian_robots: u64,
+    result_cache: &mut HashMap<MiningAssets, u64>,
+) -> u64 {
+    if mins == 0 {
+        return 0;
+    }
+    let cache_key = MiningAssets {
+        mins,
+        ore,
+        ore_robots,
+        clay,
+        clay_robots,
+        obsidian,
+        obsidian_robots,
+    };
+
+    if let Some(result) = result_cache.get(&cache_key) {
+        return *result;
+    }
+
+    let mut result = largest_num_geodes_opened_recursive(
+        blueprint,
+        mins - 1,
+        ore + ore_robots,
+        ore_robots,
+        clay + clay_robots,
+        clay_robots,
+        obsidian + obsidian_robots,
+        obsidian_robots,
+        result_cache,
+    );
+
+    if ore >= blueprint.ore_robot_cost.ore
+        && clay >= blueprint.ore_robot_cost.clay
+        && obsidian >= blueprint.ore_robot_cost.obsidian
+        && ore_robots
+            < *[
+                blueprint.ore_robot_cost.ore,
+                blueprint.clay_robot_cost.ore,
+                blueprint.obsidian_robot_cost.ore,
+                blueprint.geode_robot_cost.ore,
+            ]
+            .iter()
+            .max()
+            .unwrap()
+    {
+        result = cmp::max(
+            result,
+            largest_num_geodes_opened_recursive(
+                blueprint,
+                mins - 1,
+                ore - blueprint.ore_robot_cost.ore + ore_robots,
+                ore_robots + 1,
+                clay - blueprint.ore_robot_cost.clay + clay_robots,
+                clay_robots,
+                obsidian - blueprint.ore_robot_cost.obsidian + obsidian_robots,
+                obsidian_robots,
+                result_cache,
+            ),
+        );
+    }
+    if ore >= blueprint.clay_robot_cost.ore
+        && clay >= blueprint.clay_robot_cost.clay
+        && obsidian >= blueprint.clay_robot_cost.obsidian
+        && clay_robots
+            < *[
+                blueprint.ore_robot_cost.clay,
+                blueprint.clay_robot_cost.clay,
+                blueprint.obsidian_robot_cost.clay,
+                blueprint.geode_robot_cost.clay,
+            ]
+            .iter()
+            .max()
+            .unwrap()
+    {
+        result = cmp::max(
+            result,
+            largest_num_geodes_opened_recursive(
+                blueprint,
+                mins - 1,
+                ore - blueprint.clay_robot_cost.ore + ore_robots,
+                ore_robots,
+                clay - blueprint.clay_robot_cost.clay + clay_robots,
+                clay_robots + 1,
+                obsidian - blueprint.clay_robot_cost.obsidian + obsidian_robots,
+                obsidian_robots,
+                result_cache,
+            ),
+        );
+    }
+    if ore >= blueprint.obsidian_robot_cost.ore
+        && clay >= blueprint.obsidian_robot_cost.clay
+        && obsidian >= blueprint.obsidian_robot_cost.obsidian
+        && obsidian_robots
+            < *[
+                blueprint.ore_robot_cost.obsidian,
+                blueprint.clay_robot_cost.obsidian,
+                blueprint.obsidian_robot_cost.obsidian,
+                blueprint.geode_robot_cost.obsidian,
+            ]
+            .iter()
+            .max()
+            .unwrap()
+    {
+        result = cmp::max(
+            result,
+            largest_num_geodes_opened_recursive(
+                blueprint,
+                mins - 1,
+                ore - blueprint.obsidian_robot_cost.ore + ore_robots,
+                ore_robots,
+                clay - blueprint.obsidian_robot_cost.clay + clay_robots,
+                clay_robots,
+                obsidian - blueprint.obsidian_robot_cost.obsidian + obsidian_robots,
+                obsidian_robots + 1,
+                result_cache,
+            ),
+        );
+    }
+    if ore >= blueprint.geode_robot_cost.ore
+        && clay >= blueprint.geode_robot_cost.clay
+        && obsidian >= blueprint.geode_robot_cost.obsidian
+    {
+        result = cmp::max(
+            result,
+            (mins - 1)
+                + largest_num_geodes_opened_recursive(
+                    blueprint,
+                    mins - 1,
+                    ore - blueprint.geode_robot_cost.ore + ore_robots,
+                    ore_robots,
+                    clay - blueprint.geode_robot_cost.clay + clay_robots,
+                    clay_robots,
+                    obsidian - blueprint.geode_robot_cost.obsidian + obsidian_robots,
+                    obsidian_robots,
+                    result_cache,
+                ),
+        );
+    }
+
+    result_cache.insert(cache_key, result);
+    result
+}
+
+fn largest_num_geodes_opened(blueprint: &Blueprint, mins: u64) -> u64 {
+    let mut result_cache: HashMap<MiningAssets, u64> = HashMap::new();
+    largest_num_geodes_opened_recursive(blueprint, mins, 0, 1, 0, 0, 0, 0, &mut result_cache)
+}
+
+fn sum_of_quality_levels_of_blueprints(blueprints: Vec<Blueprint>, mins: u64) -> u64 {
     blueprints
         .iter()
         .map(|blueprint| blueprint.id * largest_num_geodes_opened(blueprint, mins))
         .sum()
 }
 
-pub fn part1(file_path: &str, mins: u32) -> u64 {
+pub fn part1(file_path: &str, mins: u64) -> u64 {
+    let start_time = SystemTime::now();
+
     let blueprints = parse_blueprints(file_path);
-    sum_of_quality_levels_of_blueprints(blueprints, mins)
+    let result = sum_of_quality_levels_of_blueprints(blueprints, mins);
+
+    println!(
+        "Time taken: {}s",
+        (start_time.elapsed().unwrap().as_millis() as f32) / 1000.0
+    );
+    result
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,7 @@
 mod day19;
 
 fn main() {
-    println!("Part 1 result (test data): {}", day19::part1("../data/day_19_test.txt", 24));
+    // println!("Part 1 result (test data): {}", day19::part1("../data/day_19_test.txt", 24));
     println!("Part 1 result (real data): {}", day19::part1("../data/day_19.txt", 24));
     // println!("Part 2 result (test data): {}", day19::part2("../data/day_19_test.txt"));
     // println!("Part 2 result (real data): {}", day19::part2("../data/day_19.txt"));
