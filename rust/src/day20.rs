b464924192a049e43a94018864d22d9292309272
thomasschafer <thomasschafer97@gmail.com>
2023-01-07 22:04:51 +0000

    add day 20 part 2 solution

--- rust/src/day20.rs
+++ rust/src/day20.rs
@@ -7,7 +7,17 @@ where
     (num % mod_by + mod_by) % mod_by
 }
 
-fn mix(numbers_with_indices: &mut Vec<(usize, i32)>) {
+fn parse_numbers(file_path: &str) -> Vec<(usize, i64)> {
+    let file_contents = fs::read_to_string(file_path).unwrap();
+
+    file_contents
+        .split('\n')
+        .map(|s| s.parse::<i64>().unwrap())
+        .enumerate()
+        .collect::<Vec<_>>()
+}
+
+fn mix(numbers_with_indices: &mut Vec<(usize, i64)>) {
     let length = numbers_with_indices.len();
 
     for start_idx in 0..length {
@@ -16,7 +26,7 @@ fn mix(numbers_with_indices: &mut Vec<(usize, i32)>) {
             .position(|&(idx, _)| idx == start_idx)
             .unwrap();
         let (_, num) = numbers_with_indices[cur_idx];
-        let mut new_idx = modulus(cur_idx as i32 + num, length as i32 - 1) as usize;
+        let mut new_idx = modulus(cur_idx as i64 + num, length as i64 - 1) as usize;
         if new_idx == 0 {
             new_idx = length - 1;
         }
@@ -37,32 +47,34 @@ fn mix(numbers_with_indices: &mut Vec<(usize, i32)>) {
     }
 }
 
-fn sum_of_grove_coordinates(numbers: Vec<i32>) -> i32 {
+fn sum_of_grove_coordinates(numbers_with_indices: Vec<(usize, i64)>) -> i64 {
+    let numbers = numbers_with_indices
+        .iter()
+        .map(|&(_, num)| num)
+        .collect::<Vec<i64>>();
     let length = numbers.len();
     let idx_of_zero = numbers.iter().position(|&a| a == 0).unwrap();
+
     numbers[(idx_of_zero + 1000) % length]
         + numbers[(idx_of_zero + 2000) % length]
         + numbers[(idx_of_zero + 3000) % length]
 }
 
-pub fn part1(file_path: &str) -> i32 {
-    let file_contents = fs::read_to_string(file_path).unwrap();
-
-    let mut numbers_with_indices = file_contents
-        .split('\n')
-        .map(|s| s.parse::<i32>().unwrap())
-        .enumerate()
-        .collect::<Vec<_>>();
+pub fn part1(file_path: &str) -> i64 {
+    let mut numbers_with_indices = parse_numbers(file_path);
 
     mix(&mut numbers_with_indices);
-    sum_of_grove_coordinates(
-        numbers_with_indices
-            .iter()
-            .map(|&(_, num)| num)
-            .collect::<Vec<i32>>(),
-    )
+    sum_of_grove_coordinates(numbers_with_indices)
 }
 
-pub fn part2(file_path: &str) -> i32 {
-    1
+pub fn part2(file_path: &str) -> i64 {
+    let mut numbers_with_indices = parse_numbers(file_path)
+        .iter()
+        .map(|&(idx, num)| (idx, num * 811589153))
+        .collect::<Vec<_>>();
+
+    for _ in 0..10 {
+        mix(&mut numbers_with_indices);
+    }
+    sum_of_grove_coordinates(numbers_with_indices)
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,6 +3,6 @@ mod day20;
 fn main() {
     println!("Part 1 result (test data): {}", day20::part1("../data/day_20_test.txt"));
     println!("Part 1 result (real data): {}", day20::part1("../data/day_20.txt"));
-    // println!("Part 2 result (test data): {}", day20::part2("../data/day_20_test.txt"));
-    // println!("Part 2 result (real data): {}", day20::part2("../data/day_20.txt"));
+    println!("Part 2 result (test data): {}", day20::part2("../data/day_20_test.txt"));
+    println!("Part 2 result (real data): {}", day20::part2("../data/day_20.txt"));
 }
