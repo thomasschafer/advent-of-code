1c97989224144fa606d246b6f91d97db8f3e8301
thomasschafer <thomasschafer97@gmail.com>
2023-01-21 17:28:31 +0000

    add day 21 part 2 solution

--- rust/src/day21.rs
+++ rust/src/day21.rs
@@ -20,22 +20,55 @@ fn calculate_number(monkey_name: String, monkeys: &HashMap<String, String>) -> i
     }
     let monkey_1_num = calculate_number(monkey_string[..4].to_string(), monkeys);
     let monkey_2_num = calculate_number(monkey_string[7..].to_string(), monkeys);
-    match monkey_string.chars().collect::<Vec<_>>()[5] {
+    let operator = monkey_string.chars().collect::<Vec<_>>()[5];
+    match operator {
         '+' => monkey_1_num + monkey_2_num,
         '-' => monkey_1_num - monkey_2_num,
         '*' => monkey_1_num * monkey_2_num,
         '/' => monkey_1_num / monkey_2_num,
-        operator => panic!("Found unexpected operator {}", operator),
+        _ => panic!("Found unexpected operator {}", operator),
+    }
+}
+
+fn build_equation(monkey_name: String, monkeys: &HashMap<String, String>) -> String {
+    if monkey_name == "humn" {
+        return "x".to_string();
+    }
+    let monkey_string = monkeys.get(&monkey_name).unwrap();
+    if monkey_string.len() <= 4 {
+        return monkey_string.clone();
+    }
+
+    let monkey_1_equation = build_equation(monkey_string[..4].to_string(), monkeys);
+    let monkey_2_equation = build_equation(monkey_string[7..].to_string(), monkeys);
+    let operator = monkey_string.chars().collect::<Vec<_>>()[5];
+
+    if !(monkey_1_equation.contains("x") || monkey_2_equation.contains("x")) {
+        let monkey_1_num = monkey_1_equation.parse::<i64>().unwrap();
+        let monkey_2_num = monkey_2_equation.parse::<i64>().unwrap();
+        let result = match operator {
+            '+' => monkey_1_num + monkey_2_num,
+            '-' => monkey_1_num - monkey_2_num,
+            '*' => monkey_1_num * monkey_2_num,
+            '/' => monkey_1_num / monkey_2_num,
+            _ => panic!("Found unexpected operator {}", operator),
+        };
+        result.to_string()
+    } else {
+        format!("({}{}{})", monkey_1_equation, operator, monkey_2_equation)
     }
 }
 
 pub fn part1(file_path: &str) -> i64 {
     let monkeys = parse_monkeys(file_path);
-    println!("Monkeys: {:?}", monkeys);
     let start_monkey = "root".to_string();
     calculate_number(start_monkey, &monkeys)
 }
 
-pub fn part2(file_path: &str) -> i64 {
-    1
+pub fn part2(file_path: &str) -> String {
+    let monkeys = parse_monkeys(file_path);
+    let root_equation = monkeys.get("root").unwrap();
+    let lhs = build_equation(root_equation[..4].to_string(), &monkeys);
+    let rhs = build_equation(root_equation[7..].to_string(), &monkeys);
+    format!("{lhs} = {rhs}")
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,12 +9,12 @@ fn main() {
         "Part 1 result (real data): {}",
         day21::part1("../data/day_21.txt")
     );
-    // println!(
-    //     "Part 2 result (test data): {}",
-    //     day21::part2("../data/day_21_test.txt")
-    // );
-    // println!(
-    //     "Part 2 result (real data): {}",
-    //     day21::part2("../data/day_21.txt")
-    // );
+    println!(
+        "Part 2 result (test data): {}",
+        day21::part2("../data/day_21_test.txt")
+    );
+    println!(
+        "Part 2 result (real data): {}",
+        day21::part2("../data/day_21.txt")
+    );
 }
