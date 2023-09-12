b94490a5e1db6c72d592db54c2ce6ba146f6df5f
thomasschafer <thomasschafer97@gmail.com>
2023-09-12 08:04:52 +0100

    add day 1 part 2 solution

--- 2021/day1.hs
+++ 2021/day1.hs
@@ -1,12 +1,23 @@
-numIncreases :: [Int] -> Int
-numIncreases depths =
+numIncreasesPt1 :: [Int] -> Int
+numIncreasesPt1 depths =
   snd $ foldl incrIfGreater (Nothing, 0) depths
   where
     incrIfGreater (Nothing, c) next = (Just next, c)
     incrIfGreater (Just prev, c) next = (Just next, c + (if next > prev then 1 else 0))
 
+numIncreasesPt2 :: Int -> [Int] -> Int
+numIncreasesPt2 windowLength depths =
+  snd $ foldl threeSum (Nothing, 0) [0 .. (length depths - windowLength)]
+  where
+    threeSum (prev, c) idx = case prev of
+      Nothing -> (Just nextSum, c)
+      (Just prevSum) -> (Just nextSum, c + (if nextSum > prevSum then 1 else 0))
+      where
+        nextSum = sum $ take windowLength $ drop idx depths
+
 main :: IO ()
 main = do
   depthsData <- readFile "data/day_1.txt"
   let depths = map read $ lines depthsData
-  print $ numIncreases depths
+  print $ numIncreasesPt1 depths
+  print $ numIncreasesPt2 3 depths
