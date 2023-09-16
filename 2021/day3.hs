3a59e414e861a509da44f407343ceb8c4cb92641
thomasschafer <thomasschafer97@gmail.com>
2023-09-16 22:40:55 +0100

    add day 3 part 2 solution

--- 2021/day3.hs
+++ 2021/day3.hs
@@ -23,7 +23,47 @@ solvePart1 diagnosticData = do
     leastFrequentChars = map (\x -> if x == '1' then '0' else '1') mostFrequentChars
     epsilonRate = binaryStringToInt leastFrequentChars
 
+filterByFreq :: ([(Int, Char)] -> Char) -> [String] -> Int -> String
+filterByFreq filterFunc diagnosticData curIdx =
+  if length filteredData <= 1
+    then head filteredData
+    else filterByFreq filterFunc filteredData (curIdx + 1)
+  where
+    counts = charCounts $ map (!! curIdx) diagnosticData
+    requiredChar = filterFunc counts
+    filteredData = filter ((== requiredChar) . (!! curIdx)) diagnosticData
+
+calcOxygenGenRating :: [String] -> Int
+calcOxygenGenRating diagnosticData =
+  binaryStringToInt $ filterOxygenGenRating diagnosticData 0
+  where
+    mostCommon counts = case counts of
+      [(_, a), (_, b)] | a == b -> '1'
+      _ -> snd $ maximum counts
+    filterOxygenGenRating = filterByFreq mostCommon
+
+calcCo2ScrubRating :: [String] -> Int
+calcCo2ScrubRating diagnosticData = binaryStringToInt $ filterCo2ScrubRating diagnosticData 0
+  where
+    leastCommon counts = case counts of
+      [(_, a), (_, b)] | a == b -> '0'
+      _ -> snd $ minimum counts
+    filterCo2ScrubRating = filterByFreq leastCommon
+
+solvePart2 :: [String] -> Int
+solvePart2 diagnosticData =
+  oxygenGenRating * co2ScrubRating
+  where
+    oxygenGenRating = calcOxygenGenRating diagnosticData
+    co2ScrubRating = calcCo2ScrubRating diagnosticData
+
+solveAndPrint :: String -> IO ()
+solveAndPrint filePath = do
+  diagnosticData <- readFile filePath
+  print $ solvePart1 $ lines diagnosticData
+  print $ solvePart2 $ lines diagnosticData
+
 main :: IO ()
 main = do
-  diagnosticData <- readFile "data/day_3.txt"
-  print $ solvePart1 $ lines diagnosticData
+  solveAndPrint "data/day_3_test.txt"
+  solveAndPrint "data/day_3.txt"
