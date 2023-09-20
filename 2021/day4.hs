23d35da7b1b19e91a5dedb905f8a4a228be967a2
thomasschafer <thomasschafer97@gmail.com>
2023-09-20 22:54:48 +0100

    complete day 4 part 1 solution

--- 2021/day4.hs
+++ 2021/day4.hs
@@ -1,4 +1,5 @@
 import Debug.Trace
+import Distribution.Simple.Setup (falseArg)
 
 type BingoBoard = [[Int]]
 
@@ -16,9 +17,9 @@ splitBy splitChar str =
   let splitFn = (== splitChar)
    in case dropWhile splitFn str of
         "" -> []
-        _ -> word : splitBy splitChar remainder
+        str' -> word : splitBy splitChar remainder
           where
-            (word, remainder) = break splitFn str
+            (word, remainder) = break splitFn str'
 
 groupBoards :: [String] -> [BingoBoard]
 groupBoards = groupBoardsHelper [] []
@@ -33,13 +34,35 @@ parseBingoData fileData = BingoData {drawnNumbers, boards}
   where
     dataLines = lines fileData
     drawnNumbers = map read $ splitBy ',' $ head dataLines
-    boards = quickTrace $ groupBoards $ drop 2 dataLines
+    boards = groupBoards $ drop 2 dataLines
 
--- TODO: implement
-solvePart1 :: BingoData -> Int
-solvePart1 bingoData = head $ drawnNumbers d
+findWinner :: [BingoBoard] -> Maybe BingoBoard
+findWinner [] = Nothing
+findWinner (board : boards) =
+  if isWinner board then Just board else findWinner boards
   where
-    d = quickTrace bingoData
+    hasCompleteRow = any (all (== 0))
+
+    hasCompleteColumn [] = False
+    hasCompleteColumn board = case head board of
+      [] -> False
+      _ -> firstColIsComplete || hasCompleteColumn (map tail board)
+        where
+          firstCol = map head board
+          firstColIsComplete = all (== head firstCol) firstCol
+
+    isWinner b = hasCompleteRow b || hasCompleteColumn b
+
+removeNum :: Int -> BingoBoard -> BingoBoard
+removeNum num = map $ map (\x -> if x == num then 0 else x)
+
+solvePart1 :: BingoData -> Int
+solvePart1 bingoData =
+  let (numCalled : remainder) = drawnNumbers bingoData
+      updatedBoards = map (removeNum numCalled) (boards bingoData)
+   in case findWinner updatedBoards of
+        Nothing -> solvePart1 $ BingoData remainder updatedBoards
+        Just winningBoard -> numCalled * sum (map sum winningBoard)
 
 solveAndPrint :: String -> IO ()
 solveAndPrint filePath = do
@@ -49,3 +72,4 @@ solveAndPrint filePath = do
 main :: IO ()
 main = do
   solveAndPrint "data/day_4_test.txt"
+  solveAndPrint "data/day_4.txt"
