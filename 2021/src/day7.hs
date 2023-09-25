9f43fd16b059efff0e715b038731f2ee9bc4bd14
thomasschafer <thomasschafer97@gmail.com>
2023-09-25 23:08:15 +0100

    add day 7 part 2 solution

--- 2021/src/day7.hs
+++ 2021/src/day7.hs
@@ -2,17 +2,29 @@ module Day7 (day7Main) where
 
 import Utils (splitBy)
 
-solvePart1 :: String -> Int
-solvePart1 positionsData = foldl calcFuelSpent fuelUpperBound [minPos .. maxPos]
+solveFuelRequired :: (Int -> Int -> Int) -> String -> Int
+solveFuelRequired fuelFromDistance positionsData =
+  foldl calcFuelSpent fuelUpperBound [minPos .. maxPos]
   where
     positions = map read $ splitBy ',' positionsData :: [Int]
     minPos = minimum positions
     maxPos = maximum positions
-    fuelUpperBound = (maxPos - minPos) * length positions
+    fuelUpperBound = fuelFromDistance maxPos minPos * length positions
 
     calcFuelSpent minFuel pos = min minFuel curFuelRequired
       where
-        curFuelRequired = sum $ map (abs . (pos -)) positions
+        curFuelRequired = sum $ map (fuelFromDistance pos) positions
+
+distance :: (Num a) => a -> a -> a
+distance = (abs .) . (-)
+
+solvePart1 :: String -> Int
+solvePart1 = solveFuelRequired distance
+
+solvePart2 :: String -> Int
+solvePart2 = solveFuelRequired $ (fuelReq .) . distance
+  where
+    fuelReq dist = dist * (dist + 1) `div` 2
 
 day7Main :: IO ()
 day7Main = do
@@ -20,3 +32,5 @@ day7Main = do
   realData <- readFile "data/day_7.txt"
   print $ solvePart1 testData
   print $ solvePart1 realData
+  print $ solvePart2 testData
+  print $ solvePart2 realData
