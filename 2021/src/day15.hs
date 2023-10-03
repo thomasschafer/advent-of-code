71105fa54b4169300aee7ba2113cdd6a2f4e848c
thomasschafer <thomasschafer97@gmail.com>
2023-10-03 23:05:49 +0100

    add day 15 part 2 solution

--- 2021/src/day15.hs
+++ 2021/src/day15.hs
@@ -21,27 +21,43 @@ listToArray2d xs =
     numRows = length xs
     numCols = if numRows == 0 then 0 else length $ head xs
 
-solvePart1 :: String -> Float
-solvePart1 riskData = finalPathRisks ! (numRows - 1, numCols - 1)
+-- Uses A* search algorithm
+findLowestRiskPath :: Array (Int, Int) Int -> Maybe Float
+findLowestRiskPath riskLevels = result
   where
-    riskLevelsList = map (map digitToInt) $ lines riskData
-    numRows = length riskLevelsList
-    numCols = length $ head riskLevelsList
-    riskLevels = listToArray2d riskLevelsList
+    ((minRow, minCol), (maxRow, maxCol)) = bounds riskLevels
+    numRows = maxRow - minRow + 1
+    numCols = maxCol - minCol + 1
 
-    initialWeight i j = if (i, j) == (0, 0) then 0 else infinity
-    initialQueue = MQ.fromList [(initialWeight i j, (i, j)) | i <- [0 .. numRows], j <- [0 .. numCols]]
     initialPathRisks = listToArray2d [[initialWeight i j | i <- [0 .. numRows]] | j <- [0 .. numCols]]
-
-    (finalPathRisks, _) = updatePathRisks initialPathRisks initialQueue
       where
-        updatePathRisks :: PathRiskLevels -> MinQueue WeightedCoord -> (PathRiskLevels, MinQueue WeightedCoord)
-        updatePathRisks pathRisks queue =
-          if MQ.null queue
-            then (pathRisks, queue)
-            else updatePathRisks updatedPathRisks updatedQueue
+        initialWeight :: Int -> Int -> Float
+        initialWeight i j
+          | (i, j) == (0, 0) = 0
+          | otherwise = infinity
+
+    heurPathRisk :: (Int, Int) -> Float
+    heurPathRisk (i, j) = sqrt ((iGoal - i') ** 2 + (jGoal - j') ** 2)
+      where
+        (i', j') = (fromIntegral i, fromIntegral j)
+        (iGoal, jGoal) = (fromIntegral (numRows - 1), fromIntegral (numCols - 1))
+
+    initialBestGuess :: Int -> Int -> Float
+    initialBestGuess i j
+      | (i, j) == (0, 0) = heurPathRisk (i, j)
+      | otherwise = infinity
+    initialQueue = MQ.singleton (initialBestGuess 0 0, (0, 0))
+    initialBestGuesses = listToArray2d [[initialBestGuess i j | i <- [0 .. numRows]] | j <- [0 .. numCols]]
+
+    result = updatePathRisks initialPathRisks initialBestGuesses initialQueue
+      where
+        updatePathRisks :: PathRiskLevels -> PathRiskLevels -> MinQueue WeightedCoord -> Maybe Float
+        updatePathRisks pathRisks bestGuesses queue
+          | MQ.null queue = Nothing
+          | (i, j) == (numRows - 1, numCols - 1) = Just $ updatedPathRisks ! (i, j) -- todo: is this correct?
+          | otherwise = updatePathRisks updatedPathRisks updatedBestGuesses updatedQueue
           where
-            ((_, (i, j)), queueWithoutMin) = MQ.deleteFindMin queue
+            ((_, current@(i, j)), queueWithoutMin) = MQ.deleteFindMin queue
             neighbors =
               [ (r, c)
                 | (r, c) <- [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)],
@@ -50,18 +66,42 @@ solvePart1 riskData = finalPathRisks ! (numRows - 1, numCols - 1)
                   c >= 0,
                   c < numCols
               ]
-            (neighborsQueue, remainingQueue) = MQ.partition ((`elem` neighbors) . snd) queueWithoutMin
-            (updatedPathRisks, updatedQueue) = MQ.foldlU updateQueue (pathRisks, remainingQueue) neighborsQueue
+            (updatedPathRisks, updatedBestGuesses, updatedQueue) = foldl updateQueue (pathRisks, bestGuesses, queueWithoutMin) neighbors
               where
-                updateQueue :: (PathRiskLevels, MinQueue WeightedCoord) -> WeightedCoord -> (PathRiskLevels, MinQueue WeightedCoord)
-                updateQueue (prevPathRisks, prevQueue) (_, (iNeigh, jNeigh)) = (newPathRisks, newQueue)
-                  where
-                    newPathRisk =
-                      min
-                        (prevPathRisks ! (iNeigh, jNeigh))
-                        ((prevPathRisks ! (i, j)) + fromIntegral (riskLevels ! (iNeigh, jNeigh)))
-                    newPathRisks = prevPathRisks // [((iNeigh, jNeigh), newPathRisk)]
-                    newQueue = MQ.insert (newPathRisk, (iNeigh, jNeigh)) prevQueue
+                updateQueue :: (PathRiskLevels, PathRiskLevels, MinQueue WeightedCoord) -> (Int, Int) -> (PathRiskLevels, PathRiskLevels, MinQueue WeightedCoord)
+                updateQueue (prevPathRisks, prevBestGuesses, prevQueue) neighbor =
+                  let tentativePathRisk = (prevPathRisks ! current) + fromIntegral (riskLevels ! neighbor)
+                   in if tentativePathRisk < (prevPathRisks ! neighbor)
+                        then
+                          let newPathRisks = prevPathRisks // [(neighbor, tentativePathRisk)]
+                              newBestGuess = tentativePathRisk + heurPathRisk neighbor
+                              newBestGuesses = prevBestGuesses // [(neighbor, newBestGuess)]
+                              newQueue = MQ.insert (newBestGuess, neighbor) $ MQ.filter ((/= neighbor) . snd) prevQueue
+                           in (newPathRisks, newBestGuesses, newQueue)
+                        else (prevPathRisks, prevBestGuesses, prevQueue)
+
+solvePart1 :: String -> Maybe Float
+solvePart1 riskData = findLowestRiskPath riskLevels
+  where
+    riskLevelsList = map (map digitToInt) $ lines riskData
+    riskLevels = listToArray2d riskLevelsList
+
+solvePart2 :: String -> Maybe Float
+solvePart2 riskData = findLowestRiskPath riskLevels
+  where
+    riskLevelsList = map (map digitToInt) $ lines riskData
+    riskLevelsTiled =
+      [ [ wrap $ riskLevelsList !! (i `mod` numRows) !! (j `mod` numCols) + (i `div` numRows) + (j `div` numCols)
+          | j <- [0 .. numTiles * numCols - 1]
+        ]
+        | i <- [0 .. numTiles * numRows - 1]
+      ]
+      where
+        wrap x = (x - 1) `mod` 9 + 1
+        numTiles = 5
+        numRows = length riskLevelsList
+        numCols = length riskLevelsList
+    riskLevels = listToArray2d riskLevelsTiled
 
 day15Main :: IO ()
 day15Main = do
@@ -69,6 +109,5 @@ day15Main = do
   realData <- readFile "data/day_15.txt"
   print $ solvePart1 testData
   print $ solvePart1 realData
-
--- print $ solvePart2 testData
--- print $ solvePart2 realData
+  print $ solvePart2 testData
+  print $ solvePart2 realData
