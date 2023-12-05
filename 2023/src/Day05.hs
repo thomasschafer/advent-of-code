945800c57bcd56cd068ab085d4f5082ff9a2e9a7
thomasschafer <thomasschafer97@gmail.com>
2023-12-05 22:51:05 +0000

    Add day 5 part 2 solution

--- 2023/src/Day05.hs
+++ 2023/src/Day05.hs
@@ -1,33 +1,77 @@
 module Day05 (day05Main) where
 
+import Data.Function (on)
+import Data.List (sortBy)
 import Data.List.Split (splitOn)
+import Utils (withIdx)
 
-type Mapping = ((Int, Int), Int) -- ((sourceStart, sourceEnd), amountToUpdate)
+data Mapping = Mapping {sourceStart :: Int, sourceEnd :: Int, amountToUpdate :: Int}
 
 parseSeedsAndMappings :: String -> ([Int], [[Mapping]])
 parseSeedsAndMappings seedData = (seeds, mappings)
   where
     seedLine : rest = splitOn "\n\n" seedData
-    seeds = map read . splitOn " " . (!! 1) $ splitOn ": " seedLine :: [Int]
-    toMapping :: [Int] -> Mapping
-    toMapping [destRangeStart, sourceRangeStart, rangeLength] =
-      ((sourceRangeStart, sourceRangeStart + rangeLength - 1), destRangeStart - sourceRangeStart)
-    mappings = map (map (toMapping . map read . splitOn " ") . tail . lines) rest :: [[Mapping]]
+    seeds = map read . splitOn " " . (!! 1) $ splitOn ": " seedLine
+    toMapping [destRangeStart, sourceStart, rangeLength] =
+      Mapping
+        { sourceStart,
+          sourceEnd = sourceStart + rangeLength - 1,
+          amountToUpdate = destRangeStart - sourceStart
+        }
+    mappings = map (map (toMapping . map read . splitOn " ") . tail . lines) rest
 
-applyMappings :: Int -> [Mapping] -> Int
-applyMappings num mappings =
-  case filter mappingContainsNum mappings of
-    [((sourceStart, sourceEnd), amountToUpdate)] -> num + amountToUpdate
-    [] -> num
-    m -> error $ "Expected to find either 0 or 1 mappings, instead found " ++ show m
+filterAndSortMappings :: (Int, Int) -> [Mapping] -> [Mapping]
+filterAndSortMappings (num, len) mappings = sortBy (compare `on` sourceStart) filteredMappings
   where
-    mappingContainsNum ((sourceStart, sourceEnd), _) = num >= sourceStart && num <= sourceEnd
+    mappingIntersects Mapping {sourceStart, sourceEnd} = num <= sourceEnd && sourceStart <= num + len - 1
+    filteredMappings = filter mappingIntersects mappings
 
-applyAllMappings :: [Int] -> [[Mapping]] -> [Int]
-applyAllMappings = foldl (\nums mappings -> map (`applyMappings` mappings) nums)
+applyMappingsToRange :: [Mapping] -> (Int, Int) -> [(Int, Int)]
+applyMappingsToRange mappings (rangeStart, len) = updatedRanges
+  where
+    orderedMappings = filterAndSortMappings (rangeStart, len) mappings
+    maybeMappingsStart = if null orderedMappings then Nothing else Just ((sourceStart . head) orderedMappings)
+    rangeEnd = rangeStart + len - 1
+    maybeMappingsEnd = if null orderedMappings then Nothing else Just ((sourceEnd . last) orderedMappings)
+
+    addMappings idx = cur : ([mappingToAdd | sourceEnd cur < sourceStart next - 1])
+      where
+        cur = orderedMappings !! idx
+        next = orderedMappings !! (idx + 1)
+        mappingToAdd = Mapping {sourceStart = sourceEnd cur + 1, sourceEnd = sourceStart next - 1, amountToUpdate = 0}
+
+    trim Mapping {sourceStart, sourceEnd, amountToUpdate} =
+      Mapping
+        { sourceStart = max sourceStart rangeStart,
+          sourceEnd = min sourceEnd rangeEnd,
+          amountToUpdate
+        }
+
+    allMappings = map trim $
+      case (maybeMappingsStart, maybeMappingsEnd) of
+        (Just mappingsStart, Just mappingsEnd) ->
+          [Mapping {sourceStart = rangeStart, sourceEnd = mappingsStart - 1, amountToUpdate = 0} | mappingsStart > rangeStart]
+            ++ concatMap addMappings [0 .. length orderedMappings - 2]
+            ++ [last orderedMappings]
+            ++ [Mapping {sourceStart = mappingsEnd + 1, sourceEnd = rangeEnd, amountToUpdate = 0} | rangeEnd > mappingsEnd]
+        _ -> [Mapping {sourceStart = rangeStart, sourceEnd = rangeEnd, amountToUpdate = 0}]
+
+    updatedRanges = map (\Mapping {sourceStart, sourceEnd, amountToUpdate} -> (sourceStart + amountToUpdate, sourceEnd - sourceStart + 1)) allMappings
+
+solve :: ([Int] -> [(Int, Int)]) -> String -> Int
+solve seedsToRanges seedData = fst . minimum $ foldl update seedRanges allMappings
+  where
+    (seeds, allMappings) = parseSeedsAndMappings seedData
+    seedRanges = seedsToRanges seeds
+    update ranges m = concatMap (applyMappingsToRange m) ranges
 
 part1 :: String -> Int
-part1 = minimum . uncurry applyAllMappings . parseSeedsAndMappings
+part1 = solve (map (,1))
+
+part2 :: String -> Int
+part2 = solve (foldl toSeedRanges [] . withIdx)
+  where
+    toSeedRanges acc (idx, num) = if odd idx then init acc ++ [(fst (last acc), num)] else acc ++ [(num, -1)]
 
 day05Main :: IO ()
 day05Main = do
@@ -35,3 +79,5 @@ day05Main = do
   realData <- readFile "data/day_5.txt"
   print $ part1 testData
   print $ part1 realData
+  print $ part2 testData
+  print $ part2 realData
