b5c702b83c6274e1de7609137d9bdc1948555946
thomasschafer <thomasschafer97@gmail.com>
2023-12-06 21:30:51 +0000

    Add day 6 part 2 solution

--- 2023/src/Day06.hs
+++ 2023/src/Day06.hs
@@ -1,15 +1,22 @@
 module Day06 (day06Main) where
 
+import Data.Char (isSpace)
 import Data.List.Split (splitOn)
 
-parseData :: String -> [(Int, Int)] -- [(time, distance)]
-parseData = (\[a, b] -> zip a b) . map (map read . filter (/= "") . tail . splitOn " ") . lines
+type TimeDistPair = (Int, Int)
+
+numWaysToWin :: TimeDistPair -> Int
+numWaysToWin (time, distToBeat) = length . filter (> distToBeat) $ map (\t -> (time - t) * t) [0 .. time]
 
 part1 :: String -> Int
-part1 = product . map numWaysToWin . parseData
+part1 = product . map numWaysToWin . parseSeparateNums
   where
-    numWaysToWin (time, distToBeat) =
-      length . filter (> distToBeat) $ map (\t -> (time - t) * t) [0 .. time]
+    parseSeparateNums = (\[a, b] -> zip a b) . map (map read . tail . words) . lines
+
+part2 :: String -> Int
+part2 = numWaysToWin . parseSingleNums
+  where
+    parseSingleNums = (\[a, b] -> (a, b)) . map (read . filter (not . isSpace) . last . splitOn ":") . lines
 
 day06Main :: IO ()
 day06Main = do
@@ -17,3 +24,5 @@ day06Main = do
   realData <- readFile "data/day_6.txt"
   print $ part1 testData
   print $ part1 realData
+  print $ part2 testData
+  print $ part2 realData
