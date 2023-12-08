2d9ee702f0b10c1534fa9d9d12d65ff873107dc6
thomasschafer <thomasschafer97@gmail.com>
2023-12-08 16:19:37 +0000

    Tidy up main func

--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -1,27 +1,20 @@
-mod day22;
-
-#[derive(PartialEq)]
-struct Foo;
+mod day21;
 
 fn main() {
-    let a = Foo;
-    let b = Foo {};
-    println!("{}", a == b);
-
-    // println!(
-    //     "Part 1 result (test data): {}",
-    //     day22::part1("../data/day_22_test.txt")
-    // );
-    // println!(
-    //     "Part 1 result (real data): {}",
-    //     day22::part1("../data/day_22.txt")
-    // );
-    // println!(
-    //     "Part 2 result (test data): {}",
-    //     day22::part2("../data/day_22_test.txt")
-    // );
-    // println!(
-    //     "Part 2 result (real data): {}",
-    //     day22::part2("../data/day_22.txt")
-    // );
+    println!(
+        "Part 1 result (test data): {}",
+        day21::part1("../data/day_22_test.txt")
+    );
+    println!(
+        "Part 1 result (real data): {}",
+        day21::part1("../data/day_22.txt")
+    );
+    println!(
+        "Part 2 result (test data): {}",
+        day21::part2("../data/day_22_test.txt")
+    );
+    println!(
+        "Part 2 result (real data): {}",
+        day21::part2("../data/day_22.txt")
+    );
 }
