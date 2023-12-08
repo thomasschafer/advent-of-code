130e8de0e2dd67d73ad58352c6fdd2edcc20dcf5
thomasschafer <thomasschafer97@gmail.com>
2023-12-08 07:28:11 +0000

    Add day 8 part 2 solution

--- 2023/src/Day08.hs
+++ 2023/src/Day08.hs
@@ -3,6 +3,7 @@ module Day08 (part1, part2) where
 import Data.HashMap.Strict (HashMap)
 import Data.HashMap.Strict qualified as HM
 import Data.Maybe (fromJust)
+import Utils (quickTrace)
 
 data Direction = L | R
   deriving (Eq, Show)
@@ -19,19 +20,29 @@ parseNodes nodeData = (directions, nodes)
     update ns (key, node) = HM.insert key node ns
     nodes = foldl update HM.empty (map (parseNode . words) nodesLines)
 
-stepsToReachEnd :: [Direction] -> Node -> HashMap String Node -> Int -> Int
-stepsToReachEnd (nextDir : rest) Node {val, left, right} nodeMapping acc =
-  if val == "ZZZ"
+stepsToReachEnd :: (String -> Bool) -> [Direction] -> Node -> HashMap String Node -> Int -> Int
+stepsToReachEnd isEnd (nextDir : rest) Node {val, left, right} nodeMapping acc =
+  if isEnd val
     then acc
-    else stepsToReachEnd rest nextNode nodeMapping (acc + 1)
+    else stepsToReachEnd isEnd rest nextNode nodeMapping (acc + 1)
   where
     nextNode = fromJust $ HM.lookup (if nextDir == L then left else right) nodeMapping
+stepsToReachEnd _ [] _ _ _ = error "Found empty directions"
 
 part1 :: String -> Int
-part1 s = stepsToReachEnd (cycle dirs) startNode nodeMapping 0
+part1 s = stepsToReachEnd (== "ZZZ") (cycle dirs) startNode nodeMapping 0
   where
     (dirs, nodeMapping) = parseNodes s
     startNode = fromJust $ HM.lookup "AAA" nodeMapping
 
+stepsForAllToReachEnd :: [Direction] -> [Node] -> HashMap String Node -> Int -> Int
+stepsForAllToReachEnd dirs nodes nodeMapping acc = foldl1 lcm steps
+  where
+    steps = quickTrace "s" $ map (\n -> stepsToReachEnd ((== 'Z') . last) dirs n nodeMapping acc) nodes
+
 part2 :: String -> Int
-part2 = const 1
+part2 s = stepsForAllToReachEnd (cycle dirs) startNodes nodeMapping 0
+  where
+    (dirs, nodeMapping) = parseNodes s
+    nodesEndingInA = filter ((== 'A') . last) (HM.keys nodeMapping)
+    startNodes = map (\key -> fromJust $ HM.lookup key nodeMapping) nodesEndingInA
--- 2023/src/Main.hs
+++ 2023/src/Main.hs
@@ -4,10 +4,10 @@ main :: IO ()
 main = do
   testData <- readFile "data/day_8_test.txt"
   testData2 <- readFile "data/day_8_test_2.txt"
+  testData3 <- readFile "data/day_8_test_3.txt"
   realData <- readFile "data/day_8.txt"
   print $ part1 testData -- 2
   print $ part1 testData2 -- 6
   print $ part1 realData
-  -- print $ part2 testData
-  -- print $ part2 realData
-  return ()
+  print $ part2 testData3 -- 6
+  print $ part2 realData
