ec020fe3d39e4240404cd72043888d28cf6020a5
thomasschafer <thomasschafer97@gmail.com>
2023-12-21 07:25:33 +0000

    (wip) Add day 21 part 2

--- 2023/src/Day21.hs
+++ 2023/src/Day21.hs
@@ -1,16 +1,23 @@
-module Day21 (part1, part2) where
+module Day21 (part1, part2, solve) where
 
+import Control.Applicative (liftA2)
+import Data.HashMap.Strict (HashMap)
+import Data.HashMap.Strict qualified as HM
 import Data.Set (Set)
 import Data.Set qualified as S
 
-plotsReachedAfter :: [String] -> Int -> Set (Int, Int)
-plotsReachedAfter plots n =
+plotsReachedAfter :: [String] -> (Bool, Int) -> Set (Int, Int)
+plotsReachedAfter plots (wrap, n) =
   (if odd n then fst else snd) $
     plotsReachedAfter' 1 initial S.empty S.empty
   where
     initial = S.fromList [(r, c) | r <- [0 .. rows - 1], c <- [0 .. cols - 1], plots !! r !! c == 'S']
     rows = length plots
     cols = length $ head plots
+    canMoveTo (r, c) =
+      if wrap
+        then plots !! (r `mod` rows) !! (c `mod` cols) /= '#'
+        else r >= 0 && r < rows && c >= 0 && c < cols && plots !! r !! c /= '#'
 
     plotsReachedAfter' k cur seenOdd seenEven
       | k > n = (seenOdd, seenEven)
@@ -19,13 +26,16 @@ plotsReachedAfter plots n =
         boundaryAfterStep = foldl (\acc -> foldr S.insert acc . neighbors) S.empty cur
         neighbors (r, c) =
           filter
-            (\(r', c') -> r' >= 0 && r' < rows && c' >= 0 && c' < cols && plots !! r' !! c' /= '#')
+            (liftA2 (&&) (`notElem` (if odd k then seenOdd else seenEven)) canMoveTo)
             [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
         updatedSeenOdd = if odd k then foldr S.insert seenOdd boundaryAfterStep else seenOdd
         updatedSeenEven = if even k then foldr S.insert seenEven boundaryAfterStep else seenEven
 
-part1 :: Int -> String -> Int
-part1 = flip $ (length .) . plotsReachedAfter . lines
+solve :: Bool -> Int -> String -> Int
+solve = curry . flip $ (length .) . plotsReachedAfter . lines
+
+part1 :: String -> Int
+part1 = solve False 64
 
 part2 :: String -> Int
-part2 = const 1
+part2 = solve True 26501365
--- 2023/src/Main.hs
+++ 2023/src/Main.hs
@@ -1,11 +1,17 @@
-import Day21 (part1, part2)
+import Day21 (part1, part2, solve)
 
 main :: IO ()
 main = do
   testData <- readFile "data/day_21_test.txt"
   realData <- readFile "data/day_21.txt"
-  print . (\res -> (res == 16, res)) $ part1 6 testData
-  print . (\res -> (res == 3646, res)) $ part1 64 realData
-  -- print $ part2 testData
+  print . (\res -> (res == 16, res)) $ solve False 6 testData
+  print . (\res -> (res == 3646, res)) $ part1 realData
+  print . (\res -> (res == 16, res)) $ solve True 6 testData
+  print . (\res -> (res == 50, res)) $ solve True 10 testData
+  print . (\res -> (res == 1594, res)) $ solve True 50 testData
+  print . (\res -> (res == 6536, res)) $ solve True 100 testData
+  print . (\res -> (res == 167004, res)) $ solve True 500 testData
+  print . (\res -> (res == 668697, res)) $ solve True 1000 testData
+  print . (\res -> (res == 16733044, res)) $ solve True 5000 testData
   -- print $ part2 realData
   return ()
