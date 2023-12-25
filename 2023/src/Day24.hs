10be00c37402bedabb8abcc81316a1b264f690ee
thomasschafer <thomasschafer97@gmail.com>
2023-12-25 18:49:36 +0000

    (wip) Add day 24 part 2

--- 2023/src/Day24.hs
+++ 2023/src/Day24.hs
@@ -13,10 +13,14 @@ parse s = ((px, py, pz), (vx, vy, vz))
 
 pathsCross :: Int -> Int -> (Coords, Velocity) -> (Coords, Velocity) -> Bool
 pathsCross lower' upper' ((px1', py1', _), (vx1', vy1', _)) ((px2', py2', _), (vx2', vy2', _))
-  | vx1 == vx2 && vy1 == vy2 || vx1 * vy2 == vx2 * vy1 = (px1, py1) == (px2, py2)
-  | otherwise = k1 >= 0 && k2 >= 0 && all (\c -> c >= lower && c <= upper) [xCrossing, yCrossing]
+  | vx1 == vx2 && vy1 == vy2 || vx1 * vy2 == vx2 * vy1 =
+      (px1, py1) == (px2, py2)
+  | otherwise =
+      (k1 >= 0 && k2 >= 0)
+        && all (\c -> c >= lower && c <= upper) [xCrossing, yCrossing]
   where
-    [px1, py1, vx1, vy1, px2, py2, vx2, vy2, lower, upper] = map toRational [px1', py1', vx1', vy1', px2', py2', vx2', vy2', lower', upper']
+    [px1, py1, vx1, vy1, px2, py2, vx2, vy2, lower, upper] =
+      map toRational [px1', py1', vx1', vy1', px2', py2', vx2', vy2', lower', upper']
     k2 = ((py2 - py1) * vx1 - (px2 - px1) * vy1) / (vx2 * vy1 - vy2 * vx1)
     k1 = if vy1 == 0 then (px2 - px1 + k2 * vx2) / vx1 else (py2 - py1 + k2 * vy2) / vy1
     xCrossing = px1 + k1 * vx1
@@ -39,5 +43,16 @@ part1Test = part1Solve 7 27
 part1Real :: String -> Int
 part1Real = part1Solve 200000000000000 400000000000000
 
+collisionPosition :: [(Coords, Velocity)] -> Coords
+collisionPosition hailstones = (1, 2, 3)
+
 part2 :: String -> Int
-part2 = const 1
+part2 = addCoords . collisionPosition . map parse . lines
+  where
+    addCoords (x, y, z) = x + y + z
+
+-- co, vo, (k1, ..., kn) such that:
+-- co + k1 * vo == c1 + k1 * v1
+-- co + k2 * vo == c2 + k2 * v2
+-- ...
+-- co + kn * vo == c2 + k2 * v2
