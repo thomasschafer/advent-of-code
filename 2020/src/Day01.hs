6ad07b796c7d789b6d74ff3b0ffe762cb84cb19b
thomasschafer <thomasschafer97@gmail.com>
2024-02-20 09:24:53 +0000

    Tidy up day 1 part 2

--- 2020/src/Day01.hs
+++ 2020/src/Day01.hs
@@ -3,36 +3,25 @@ module Day01 (part1, part2) where
 import Data.Maybe (fromJust)
 import Data.Set qualified as S
 
-twoSum :: Int -> [Int] -> (Int, Int)
-twoSum target = fromJust . twoSum' S.empty
+twoSum :: (Ord a, Num a) => S.Set a -> a -> [a] -> Maybe (a, a)
+twoSum _ _ [] = Nothing
+twoSum seen target (x : xs)
+  | required `elem` seen = Just (x, required)
+  | otherwise = twoSum (S.insert x seen) target xs
   where
-    twoSum' _ [] = Nothing
-    twoSum' seen (x : xs)
-      | required `elem` seen = Just (x, required)
-      | otherwise = twoSum' (S.insert x seen) xs
-      where
-        required = target - x
+    required = target - x
 
 part1 :: String -> Int
-part1 = uncurry (*) . twoSum 2020 . map read . lines
+part1 = uncurry (*) . fromJust . twoSum S.empty 2020 . map read . lines
 
--- This solution assumes that there are no duplicates, which works for my input data.
--- If duplicates might be present we can use a hashmap with a count rather than a set.
-threeSum :: Int -> [Int] -> (Int, Int, Int)
-threeSum target nums =
-  head
-    [ (nums !! i, nums !! j, required)
-      | i <- [1 .. length nums - 1],
-        let a = nums !! i,
-        j <- [0 .. i - 1],
-        let b = nums !! j,
-        let required = target - (a + b),
-        required `elem` numsSet
-    ]
-  where
-    numsSet = S.fromList nums
+threeSum :: (Ord a, Num a) => a -> [a] -> Maybe (a, a, a)
+threeSum target (n : nums) =
+  case twoSum S.empty (target - n) nums of
+    Just (x, y) -> Just (n, x, y)
+    Nothing -> threeSum target nums
+threeSum _ _ = Nothing
 
 part2 :: String -> Int
-part2 = tupleProduct . threeSum 2020 . map read . lines
+part2 = tupleProduct . fromJust . threeSum 2020 . map read . lines
   where
     tupleProduct (x, y, z) = x * y * z
