e4e1d4cdfc7ede1477030dd047d273becc292e34
thomasschafer <thomasschafer97@gmail.com>
2024-02-24 17:01:29 +0000

    Add day 3 part 2

--- 2020/src/Day03.hs
+++ 2020/src/Day03.hs
@@ -3,19 +3,28 @@ module Day03 (part1, part2) where
 data Position = Empty | Tree
   deriving (Eq)
 
-traverseGrid :: Int -> Int -> Int -> [[Position]] -> Int
-traverseGrid row col acc grid =
-  if row >= length grid
-    then acc
-    else traverseGrid (row + 1) ((col + 3)  `mod` length (head grid)) updatedAcc grid
+traverseGrid :: (Int, Int) -> [[Position]] -> Int
+traverseGrid step@(rowStep, colStep) = traverseGrid' (0, 0) 0
   where
-    updatedAcc = acc + if (grid !! row !! col) == Tree then 1 else 0
+    traverseGrid' (row, col) acc grid =
+      if row >= length grid
+        then acc
+        else traverseGrid' updatedCoords updatedAcc grid
+      where
+        updatedAcc = acc + if (grid !! row !! col) == Tree then 1 else 0
+        updatedCoords = (row + rowStep, (col + colStep) `mod` length (head grid))
+
+parse :: Char -> Position
+parse '#' = Tree
+parse '.' = Empty
 
 part1 :: String -> Int
-part1 = traverseGrid 0 0 0 . map (map parse) . lines
-  where
-    parse '#' = Tree
-    parse '.' = Empty
+part1 = traverseGrid (1, 3) . map (map parse) . lines
 
 part2 :: String -> Int
-part2 = const 2
+part2 =
+  product
+    . flip map [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+    . flip traverseGrid
+    . map (map parse)
+    . lines
