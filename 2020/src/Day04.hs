7a709b4490545db31c79e90d21d34a7254292cd2
thomasschafer <thomasschafer97@gmail.com>
2024-02-24 23:32:17 +0000

    Add day 4 part 2

--- 2020/src/Day04.hs
+++ 2020/src/Day04.hs
@@ -2,15 +2,53 @@ module Day04 (part1, part2) where
 
 import Data.List.Split (splitOn)
 
-isValidPassport :: [(String, String)] -> Bool
-isValidPassport fields = all (`elem` map fst fields) expected
+type Passport = [(String, String)]
+
+solve :: (Passport -> Bool) -> String -> Int
+solve p = length . filter p . map parsePassport . splitOn "\n\n"
+  where
+    parsePassport = map ((\[k, v] -> (k, v)) . splitOn ":") . words
+
+isValidPassport1 :: Passport -> Bool
+isValidPassport1 passport = all (`elem` map fst passport) expected
   where
     expected = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
 part1 :: String -> Int
-part1 = length . filter isValidPassport . map parsePassport . splitOn "\n\n"
+part1 = solve isValidPassport1
+
+between :: String -> Int -> Int -> Bool
+between numStr lower upper = num >= lower && num <= upper
   where
-    parsePassport = map ((\[k, v] -> (k, v)) . splitOn ":") . words
+    num = read numStr
+
+lookupBetween :: Passport -> String -> Int -> Int -> Bool
+lookupBetween passport key lower upper = case lookup key passport of
+  Just s -> between s lower upper
+  Nothing -> False
+
+isValidPassport2 :: Passport -> Bool
+isValidPassport2 passport =
+  and
+    [ lookupBetween passport "byr" 1920 2002,
+      lookupBetween passport "iyr" 2010 2020,
+      lookupBetween passport "eyr" 2020 2030,
+      case lookup "hgt" passport of
+        Just hgt -> case splitAt (length hgt - 2) hgt of
+          (numStr, "cm") -> between numStr 150 193
+          (numStr, "in") -> between numStr 59 76
+          _ -> False
+        Nothing -> False,
+      case lookup "hcl" passport of
+        Just ('#' : rest) -> length rest == 6 && all (`elem` (['0' .. '9'] ++ ['a' .. 'f'])) rest
+        _ -> False,
+      case lookup "ecl" passport of
+        Just ecl -> ecl `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+        Nothing -> False,
+      case lookup "pid" passport of
+        Just nums -> length nums == 9 && all (`elem` ['0' .. '9']) nums
+        _ -> False
+    ]
 
 part2 :: String -> Int
-part2 = const 2
+part2 = solve isValidPassport2
--- 2020/src/Main.hs
+++ 2020/src/Main.hs
@@ -3,8 +3,9 @@ import Day04 (part1, part2)
 main :: IO ()
 main = do
   testData <- readFile "data/day_4_test.txt"
+  testData2 <- readFile "data/day_4_test_2.txt"
   realData <- readFile "data/day_4.txt"
   print $ part1 testData
   print $ part1 realData
-  print $ part2 testData
+  print $ part2 testData2
   print $ part2 realData
