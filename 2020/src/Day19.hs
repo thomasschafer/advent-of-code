de14b8abf63930928a68190bc3d541aecbbd880f
thomasschafer <thomasschafer97@gmail.com>
2024-04-18 12:41:25 +0200

    Add day 19 part 1 solution

--- 2020/src/Day19.hs
+++ 2020/src/Day19.hs
@@ -6,13 +6,13 @@ import Data.HashMap.Strict (HashMap)
 import Data.HashMap.Strict qualified as HM
 import Data.List.Split (splitOn)
 import Data.Maybe (fromJust)
-import Utils (toTuple)
+import Utils (toTuple, (...))
 
 data Rule
   = Val Char
   | Other Int
   | List [Rule]
-  | Or Rule Rule
+  | Or [Rule]
   deriving (Show)
 
 parse :: String -> (HashMap Int Rule, [String])
@@ -20,18 +20,27 @@ parse = first parseRulesMap . toTuple . map lines . splitOn "\n\n"
  where
   parseRulesMap = HM.fromList . map (bimap read (parseRules . words) . toTuple . splitOn ": ")
 
+  parseRules ['"' : c : ['"']] = Val c
   parseRules rs
-    | "|" `elem` rs = uncurry Or . toTuple . map parseRules $ splitOn ["|"] rs
-    | length rs == 1 && length (head rs) == 3 && head (head rs) == '"' = Val $ head rs !! 1
+    | "|" `elem` rs = Or . map parseRules $ splitOn ["|"] rs
     | otherwise = List $ map (Other . read) rs
 
-satisfiesRule :: Rule -> String -> Bool
-satisfiesRule = undefined
+satisfiesRule :: HashMap Int Rule -> Rule -> String -> Bool
+satisfiesRule rulesMap = ([] `elem`) ... satisfiesRule'
+ where
+  -- return value of satisfiesRule' is a list of the results of successful matching
+  satisfiesRule' (Val c) s = [tail s | not (null s) && head s == c]
+  satisfiesRule' (Or rules) s = concatMap (`satisfiesRule'` s) rules
+  satisfiesRule' (List []) s = [s]
+  satisfiesRule' (List (rule : rules)) s =
+    concatMap (satisfiesRule' $ List rules) $
+      satisfiesRule' rule s
+  satisfiesRule' (Other ruleNum) s = (`satisfiesRule'` s) . fromJust $ HM.lookup ruleNum rulesMap
 
 part1 :: String -> Int
-part1 s = length $ filter (satisfiesRule . fromJust $ HM.lookup 0 rules) strs
+part1 s = length $ filter (satisfiesRule rulesMap . fromJust $ HM.lookup 0 rulesMap) strs
  where
-  (rules, strs) = parse s
+  (rulesMap, strs) = parse s
 
 part2 :: String -> Int
 part2 = const 2
--- 2020/src/Main.hs
+++ 2020/src/Main.hs
@@ -5,7 +5,7 @@ main = do
   testData <- readFile "data/day_19_test.txt"
   realData <- readFile "data/day_19.txt"
   print $ part1 testData
-  -- print $ part1 realData
+  print $ part1 realData
   -- print $ part2 testData
   -- print $ part2 realData
   return ()
