a492e6da3e91e54e3186afb3450f068fb1929b9e
thomasschafer <thomasschafer97@gmail.com>
2024-05-17 10:20:55 +0100

    Complete day 23 part 2

--- 2020/src/Day23.hs
+++ 2020/src/Day23.hs
@@ -9,48 +9,23 @@ import Data.Vector.Mutable (MVector)
 import Data.Vector.Mutable qualified as MV
 import Utils (toInt)
 
--- move :: [Int] -> [Int]
--- move (current : cups) = tail newOrder ++ [head newOrder]
---  where
---   threeCups = take 3 cups
---   rest = drop 3 cups
---   destination = case filter (< current) rest of
---     [] -> maximum rest
---     cs -> maximum cs
---   insertionIdx = fromJust (elemIndex destination rest) + 1
---   newOrder = current : take insertionIdx rest ++ threeCups ++ drop insertionIdx rest
--- move cups = error $ "Not enough cups: " ++ show cups
---
--- orderAfter1 :: [Int] -> [Int]
--- orderAfter1 cups = drop (idx + 1) cups ++ take idx cups
---  where
---   idx = fromJust $ elemIndex 1 cups
---
--- solve :: Int -> Int -> String -> [Int]
--- solve repetitions expandedLength cupStr = orderAfter1 . (!! repetitions) $ iterate move cupsExpanded
---  where
---   cups = map toInt cupStr
---   cupsExpanded = cups ++ [(maximum cups + 1) .. expandedLength]
---
--- part1 :: String -> String
--- part1 cups = concatMap show $ solve 100 (length cups) cups
---
--- part2 :: String -> Int
--- part2 = uncurry (*) . toTuple . take 2 . join solve (10 ^ 6)
-
 -- The head of the returned vector will be the current element.
 -- For all other elements, value n being an index m means that n is immediately clockwise of m
-toMVec :: String -> ST s (MVector s Int)
-toMVec cupStr = do
-  let cups = map toInt cupStr
+toMVec :: [Int] -> ST s (MVector s Int)
+toMVec cups = do
   let pairs = zip (last cups : init cups) cups
   v <- V.thaw $ V.fromList [0 .. length cups]
   forM_ pairs $ uncurry (MV.write v)
   MV.write v 0 (head cups)
   return v
 
-move :: MVector s Int -> ST s (MVector s Int)
-move mv = do
+fromLinked :: [Int] -> [Int]
+fromLinked (current : cups) = take (length cups) $ display' current
+ where
+  display' idx = idx : display' (cups !! (idx - 1))
+
+move :: Int -> MVector s Int -> ST s (MVector s Int)
+move mvLen mv = do
   current <- MV.read mv 0
   c1 <- MV.read mv current
   c2 <- MV.read mv c1
@@ -58,7 +33,7 @@ move mv = do
   let selection = [c1, c2, c3]
   let destination =
         case maximum [current' | i <- [1 .. 4], let current' = current - i, current' `notElem` selection] of
-          0 -> MV.length mv - 1
+          0 -> maximum $ filter (`notElem` selection) [mvLen - 3 .. mvLen - 1]
           c -> c
   c3Next <- MV.read mv c3
   MV.write mv current c3Next -- current -> c3 next
@@ -69,18 +44,28 @@ move mv = do
   return mv
 
 applyMoveNTimes :: Int -> MV.MVector s Int -> ST s (MV.MVector s Int)
-applyMoveNTimes 0 mvec = return mvec
-applyMoveNTimes n mvec = move mvec >>= applyMoveNTimes (n - 1)
+applyMoveNTimes k mv = applyMoveNTimes' k mv
+ where
+  mvLen = MV.length mv
+  applyMoveNTimes' 0 mvec = return mvec
+  applyMoveNTimes' n mvec = move mvLen mvec >>= applyMoveNTimes (n - 1)
 
-orderAfter1 :: MVector s Int -> ST s [Int]
-orderAfter1 mv = return []
+orderAfter1 :: [Int] -> [Int]
+orderAfter1 nums = drop (idx1 + 1) nums ++ take (idx1 - 1) nums
+ where
+  idx1 = fromJust $ elemIndex 1 nums
 
-part1 :: String -> [Int]
-part1 cupStr = runST $ do
-  cups <- toMVec cupStr
-  cups' <- applyMoveNTimes 2 cups
-  -- orderAfter1 . V.toList <$> V.freeze cups'
-  V.toList <$> V.freeze cups'
+solve :: ([Int] -> [Int]) -> Int -> String -> [Int]
+solve transform numMoves cupStr = runST $ do
+  cups <- toMVec . transform $ map toInt cupStr
+  cupsMoved <- applyMoveNTimes numMoves cups
+  orderAfter1 . fromLinked . V.toList <$> V.freeze cupsMoved
+
+part1 :: String -> String
+part1 = concatMap show . solve id 100
 
 part2 :: String -> Int
-part2 = const 1 -- TODO
+part2 = productOfFirst2 . solve extend 10000000
+ where
+  productOfFirst2 = (\(a : b : _) -> a * b)
+  extend nums = nums ++ [length nums + 1 .. 1000000]
