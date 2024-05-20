f713eea9bb36006f8ae58d930cb30dbfb5b2cee8
thomasschafer <thomasschafer97@gmail.com>
2024-05-20 13:19:51 +0100

    Add day 25

---
+++ 2020/src/Day25.hs
@@ -0,0 +1,17 @@
+module Day25 (part1) where
+
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
+import Utils (toTuple)
+
+transform :: Integer -> Integer -> Integer
+transform subjectNum x = (x * subjectNum) `mod` 20201227
+
+encryptionKey :: Integer -> Integer -> Integer
+encryptionKey pubKey1 pubKey2 = privateKey
+ where
+  loopSize2 = fromJust . elemIndex pubKey2 $ iterate (transform 7) 1
+  privateKey = (!! loopSize2) $ iterate (transform pubKey1) 1
+
+part1 :: String -> Integer
+part1 = uncurry encryptionKey . toTuple . map read . lines
