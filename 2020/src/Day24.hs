d41d2324ee7a7361562ac6e6c6f999eaaa22a5c6
thomasschafer <thomasschafer97@gmail.com>
2024-05-20 09:57:58 +0100

    Add day 24 part 2

--- 2020/src/Day24.hs
+++ 2020/src/Day24.hs
@@ -1,29 +1,61 @@
 module Day24 (part1, part2) where
 
+import Data.List (intercalate)
+import Data.Set (Set)
 import Data.Set qualified as S
 
 -- Distance (north, east)
 -- e or w moves two units in given direction, ne/nw/se/sw moves one unit in both given directions
 type Coords = (Int, Int)
 
+moveEast, moveWest, moveSouthEast, moveSouthWest, moveNorthEast, moveNorthWest :: Coords -> Coords
+moveEast (n, e) = (n, e + 2)
+moveWest (n, e) = (n, e - 2)
+moveSouthEast (n, e) = (n - 1, e + 1)
+moveSouthWest (n, e) = (n - 1, e - 1)
+moveNorthEast (n, e) = (n + 1, e + 1)
+moveNorthWest (n, e) = (n + 1, e - 1)
+
 parsePosition :: String -> Coords
 parsePosition = parsePosition' (0, 0)
  where
-  parsePosition' coords@(distNorth, distEast) = \case
-    ('e' : rest) -> parsePosition' (distNorth, distEast + 2) rest
-    ('w' : rest) -> parsePosition' (distNorth, distEast - 2) rest
-    ('s' : 'e' : rest) -> parsePosition' (distNorth - 1, distEast + 1) rest
-    ('s' : 'w' : rest) -> parsePosition' (distNorth - 1, distEast - 1) rest
-    ('n' : 'e' : rest) -> parsePosition' (distNorth + 1, distEast + 1) rest
-    ('n' : 'w' : rest) -> parsePosition' (distNorth + 1, distEast - 1) rest
+  parsePosition' coords = \case
+    ('e' : rest) -> parsePosition' (moveEast coords) rest
+    ('w' : rest) -> parsePosition' (moveWest coords) rest
+    ('s' : 'e' : rest) -> parsePosition' (moveSouthEast coords) rest
+    ('s' : 'w' : rest) -> parsePosition' (moveSouthWest coords) rest
+    ('n' : 'e' : rest) -> parsePosition' (moveNorthEast coords) rest
+    ('n' : 'w' : rest) -> parsePosition' (moveNorthWest coords) rest
     [] -> coords
 
-part1 :: String -> Int
-part1 = length . foldl updateCounts S.empty . map parsePosition . lines
+neighbors :: Coords -> [Coords]
+neighbors = (`map` [moveEast, moveWest, moveSouthEast, moveSouthWest, moveNorthEast, moveNorthWest]) . flip ($)
+
+flipTiles :: Set Coords -> Set Coords
+flipTiles blackTiles = remainingBlackTiles `S.union` newBlackTiles
  where
-  updateCounts seen coords
+  blackNeighbors = length . filter (`elem` blackTiles) . neighbors
+
+  remainingBlackTiles = S.filter ((`elem` [1, 2]) . blackNeighbors) blackTiles
+
+  newBlackTiles =
+    S.filter (\tile -> tile `notElem` blackTiles && blackNeighbors tile == 2) $
+      foldl (flip $ S.union . S.fromList . neighbors) S.empty blackTiles
+
+solve :: String -> [Int]
+solve =
+  map length
+    . iterate flipTiles
+    . foldl updateBlackTiles S.empty
+    . map parsePosition
+    . lines
+ where
+  updateBlackTiles seen coords
     | coords `elem` seen = S.delete coords seen
     | otherwise = S.insert coords seen
 
+part1 :: String -> Int
+part1 = head . solve
+
 part2 :: String -> Int
-part2 = const 2
+part2 = (!! 100) . solve
