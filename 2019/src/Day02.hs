2195cd4ba2dadf92897cd32fe42f510723c17dea
thomasschafer <thomasschafer97@gmail.com>
2024-06-02 09:12:55 +0100

    Fix potential bug with keeping initial and current state

--- 2019/src/Day02.hs
+++ 2019/src/Day02.hs
@@ -1,23 +1,22 @@
 module Day02 (part1, part2) where
 
-import Control.Monad (join)
 import Data.List.Split (splitOn)
 
 updateAt :: Int -> a -> [a] -> [a]
 updateAt idx newVal xs = take idx xs ++ [newVal] ++ drop (idx + 1) xs
 
-solve :: [Int] -> [Int] -> [Int]
-solve cur = \case
-  (99 : _) -> cur
-  (opNum : val1 : val2 : writePos : rest) ->
-    solve (updateAt writePos (op (cur !! val1) (cur !! val2)) cur) rest
+solve :: Int -> [Int] -> [Int]
+solve curIdx opCodes = case drop curIdx opCodes of
+  (99 : _) -> opCodes
+  (opNum : val1 : val2 : writePos : _) ->
+    solve (curIdx + 4) (updateAt writePos (op (opCodes !! val1) (opCodes !! val2)) opCodes)
    where
     op = case opNum of
       1 -> (+)
       2 -> (*)
 
 part1 :: String -> Int
-part1 = head . join solve . restoreProgram . map read . splitOn ","
+part1 = head . solve 0 . restoreProgram . map read . splitOn ","
  where
   restoreProgram = updateAt 2 2 . updateAt 1 12
 
