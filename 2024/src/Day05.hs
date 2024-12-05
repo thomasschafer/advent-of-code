c8fb606bd15dbf28219fc964d5dcad138f29c076
thomasschafer <thomasschafer97@gmail.com>
2024-12-05 11:18:18 +0000

    Add day 5 part 2

--- 2024/src/Day05.hs
+++ 2024/src/Day05.hs
@@ -1,14 +1,17 @@
 module Day05 (part1, part2) where
 
+import Control.Arrow (Arrow (first, second))
 import Data.HashMap.Strict (HashMap)
 import Data.HashMap.Strict qualified as HM
 import Data.List.Split (splitOn)
 import Data.Maybe (fromMaybe)
 import Data.Set (Set)
 import Data.Set qualified as S
-import Utils (toTuple)
+import Utils (toTuple, (...))
 
-parse :: [String] -> [String] -> (HashMap Int (Set Int), [[Int]])
+type Rules = HashMap Int (Set Int)
+
+parse :: [String] -> [String] -> (Rules, [[Int]])
 parse rules updates =
   ( foldl update HM.empty $ map (toTuple . map read . splitOn "|") rules,
     map (map read . splitOn ",") updates
@@ -20,7 +23,7 @@ parse rules updates =
         (before `S.insert` fromMaybe S.empty (HM.lookup after acc))
         acc
 
-satisfiesRules :: HashMap Int (Set Int) -> [Int] -> Bool
+satisfiesRules :: Rules -> [Int] -> Bool
 satisfiesRules rules = satisfiesRules' S.empty
   where
     satisfiesRules' _ [] = True
@@ -28,11 +31,42 @@ satisfiesRules rules = satisfiesRules' S.empty
       | update `elem` notPermitted = False
       | otherwise = satisfiesRules' (notPermitted `S.union` fromMaybe S.empty (HM.lookup update rules)) rest
 
-part1 :: String -> Int
-part1 s = sum . map middle $ filter (satisfiesRules rules) updates
+stripRules :: Rules -> [Int] -> Rules
+stripRules rules xs = flip addMissing xs $ foldl update HM.empty (HM.toList rules)
+  where
+    update acc (k, v)
+      | k `elem` xs = HM.insert k (S.filter (`elem` xs) v) acc
+      | otherwise = acc
+    addMissing = foldl $ \acc x ->
+      if x `HM.member` acc
+        then acc
+        else HM.insert x S.empty acc
+
+fix :: Rules -> [Int] -> [Int]
+fix = fix' ... stripRules
+  where
+    fix' relevantRules = if null relevantRules then [] else start ++ end
+      where
+        (forEnd, forStart) = partition relevantRules
+        partition =
+          foldl
+            (flip (\(k, v) -> (if null v then first else second) (HM.insert k v)))
+            (HM.empty, HM.empty)
+            . HM.toList
+        end = map fst $ HM.toList forEnd
+        start = fix' . stripRules relevantRules $ HM.keys forStart
+
+solve :: Bool -> String -> Int
+solve fixUnordered s =
+  sum
+    . map (middle . (if fixUnordered then fix rules else id))
+    $ filter ((if fixUnordered then not else id) . satisfiesRules rules) updates
   where
     middle xs = xs !! (length xs `div` 2)
     (rules, updates) = uncurry parse . toTuple . map lines $ splitOn "\n\n" s
 
+part1 :: String -> Int
+part1 = solve False
+
 part2 :: String -> Int
-part2 = const 2
+part2 = solve True
