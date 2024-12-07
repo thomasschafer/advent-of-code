910329296339e7cbd02eef23b6e7738ffdf0b752
thomasschafer <thomasschafer97@gmail.com>
2024-12-07 07:28:52 +0000

    Add day 7 part 2

--- 2024/src/Day07.hs
+++ 2024/src/Day07.hs
@@ -4,17 +4,26 @@ import Data.Bifunctor (Bifunctor (bimap))
 import Data.List.Split (splitOn)
 import Utils (toTuple)
 
-hasSolution :: (Int, [Int]) -> Bool
-hasSolution (result, num : nums) = hasSolution' num nums
+concatOp :: Int -> Int -> Int
+concatOp x y = read $ show x ++ show y
+
+hasSolution :: Bool -> (Int, [Int]) -> Bool
+hasSolution allowConcat (result, num : nums) = hasSolution' num nums
   where
-    hasSolution' :: Int -> [Int] -> Bool
+    ops = (if allowConcat then (concatOp :) else id) [(*), (+)]
+
     hasSolution' acc [] = result == acc
     hasSolution' acc (n : ns)
       | acc > result = False
-      | otherwise = or [hasSolution' (acc `op` n) ns | op <- [(*), (+)]]
+      | otherwise = any (\op -> hasSolution' (acc `op` n) ns) ops
+
+solve :: Bool -> String -> Int
+solve allowConcat = sum . map fst . filter (hasSolution allowConcat) . parse
+  where
+    parse = map (bimap read (map read . words) . toTuple . splitOn ": ") . lines
 
 part1 :: String -> Int
-part1 = sum . map fst . filter hasSolution . map (bimap read (map read . words) . toTuple . splitOn ": ") . lines
+part1 = solve False
 
 part2 :: String -> Int
-part2 = const 2
+part2 = solve True
