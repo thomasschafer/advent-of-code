6dae514a86bef272b8b9d2ac0ac22d126eeb8b6f
thomasschafer <thomasschafer97@gmail.com>
2024-12-08 11:10:08 +0000

    Add day 8 part 2

--- 2024/src/Day08.hs
+++ 2024/src/Day08.hs
@@ -1,6 +1,5 @@
 module Day08 (part1, part2) where
 
-import Data.Set (Set)
 import Data.Set qualified as S
 import Utils (groupBy)
 
@@ -8,20 +7,27 @@ pairs :: [a] -> [(a, a)]
 pairs [] = []
 pairs (x : xs) = map (x,) xs ++ pairs xs
 
-part1 :: String -> Int
-part1 s = length . foldl1 S.union . map (antinodes . map fst) $ groupBy snd coords
+solve :: Bool -> String -> Int
+solve constrainDist s = length . foldl1 S.union . map (antinodes . map fst) $ groupBy snd coords
   where
     antennaMap = lines s
     (rows, cols) = (length antennaMap, length (head antennaMap))
     coords = [((row, col), char) | row <- [0 .. rows - 1], col <- [0 .. cols - 1], let char = antennaMap !! row !! col, char /= '.']
+    withinBounds (r, c) = r >= 0 && r < rows && c >= 0 && c < cols
 
-    antinodes :: [(Int, Int)] -> Set (Int, Int)
     antinodes = foldl1 S.union . map (S.fromList . uncurry antinodes') . pairs
       where
-        withinBounds (r, c) = r >= 0 && r < rows && c >= 0 && c < cols
-        antinodes' (r1, c1) (r2, c2) = filter withinBounds [(r1 - dr, c1 - dc), (r2 + dr, c2 + dc)]
+        antinodes' (r1, c1) (r2, c2) = filter withinBounds [(r1 + n * dr, c1 + n * dc) | n <- distFromFirst]
           where
             (dr, dc) = (r2 - r1, c2 - c1)
+            distFromFirst
+              | constrainDist = [-1, 2]
+              | otherwise = [-m .. m]
+              where
+                m = max rows cols
+
+part1 :: String -> Int
+part1 = solve True
 
 part2 :: String -> Int
-part2 = const 2
+part2 = solve False
--- 2024/src/Main.hs
+++ 2024/src/Main.hs
@@ -3,10 +3,8 @@ import Day08 (part1, part2)
 main :: IO ()
 main = do
   testData <- readFile "data/day_08_test.txt"
-  testData2 <- readFile "data/day_08_test_2.txt"
   realData <- readFile "data/day_08.txt"
   print $ part1 testData
-  print $ part1 testData2
   print $ part1 realData
   print $ part2 testData
   print $ part2 realData
