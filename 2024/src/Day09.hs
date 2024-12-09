3f434761a13490eab81cffc3d1760839c1ab4321
thomasschafer <thomasschafer97@gmail.com>
2024-12-09 10:02:11 +0000

    Add day 9 part 1

--- 2024/src/Day09.hs
+++ 2024/src/Day09.hs
@@ -2,29 +2,46 @@ module Day09 (part1, part2) where
 
 import Data.Maybe (catMaybes, isJust, mapMaybe)
 
-parse :: String -> [Maybe Int]
-parse = concatMap expand . zip [0 ..] . pairs . map (read . (: [])) . filter (/= '\n')
+parse :: String -> [(Int, Maybe Int)]
+parse = concatMap f . zip [0 ..] . pairs . map (read . (: [])) . filter (/= '\n')
   where
+    f (idx, (files, freeSpace)) = [(files, Just idx), (freeSpace, Nothing)]
     pairs [files] = [(files, 0)]
     pairs (files : freeSpace : rest) = (files, freeSpace) : pairs rest
 
-    expand (idx, (files, freeSpace)) = replicate files (Just idx) ++ replicate freeSpace Nothing
-
-moveFiles' :: [Maybe Int] -> [Int] -> [Maybe Int]
-moveFiles' [] _ = []
-moveFiles' (Nothing : rest) (end : endRest) = Just end : moveFiles' rest endRest
-moveFiles' (Just x : rest) ends = Just x : moveFiles' rest ends
-
-moveFiles :: [Maybe Int] -> [Maybe Int]
-moveFiles xs = moveFiles' (take n xs) (reverse . catMaybes $ drop n xs)
+moveFilesFrag :: [Maybe Int] -> [Maybe Int]
+moveFilesFrag files = moveFilesFrag' (take n files) (reverse . catMaybes $ drop n files)
   where
-    n = length $ filter isJust xs
+    n = length $ filter isJust files
+
+    moveFilesFrag' [] _ = []
+    moveFilesFrag' (Nothing : rest) (end : endRest) = Just end : moveFilesFrag' rest endRest
+    moveFilesFrag' (Just x : rest) ends = Just x : moveFilesFrag' rest ends
+
+pullFileForward :: [(Int, Maybe Int)] -> (Int, Int) -> [(Int, Maybe Int)]
+pullFileForward files (count, fileId) = update' files
+  where
+    update' [] = []
+    update' (cur@(c, Nothing) : rest)
+      | c >= count = ((count, Just fileId) :) . ((c - count, Nothing) :) . flip map rest $ \case
+          (c', Just f) | f == fileId -> (c', Nothing)
+          x -> x
+      | otherwise = cur : update' rest
+    update' (cur@(_, Just f) : rest)
+      | f == fileId = cur : rest
+      | otherwise = cur : update' rest
+
+moveFilesWhole :: [(Int, Maybe Int)] -> [(Int, Maybe Int)]
+moveFilesWhole files = foldl pullFileForward files . reverse . flip mapMaybe files $ \case
+  (_, Nothing) -> Nothing
+  (count, Just fileId) -> Just (count, fileId)
+
+flatten :: [(Int, Maybe Int)] -> [Maybe Int]
+flatten = concatMap (uncurry replicate)
 
 checksum :: [Maybe Int] -> Int
 checksum = sum . mapMaybe (\(idx, x) -> (idx *) <$> x) . zip [0 ..]
 
-part1 :: String -> Int
-part1 = checksum . moveFiles . parse
-
-part2 :: String -> Int
-part2 = const 2
+part1, part2 :: String -> Int
+part1 = checksum . moveFilesFrag . flatten . parse
+part2 = checksum . flatten . moveFilesWhole . parse
