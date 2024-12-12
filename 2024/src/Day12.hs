8292c391b98205fb1213e86294bfa99c75b7cc9e
thomasschafer <thomasschafer97@gmail.com>
2024-12-12 12:00:42 +0000

    Add day 12 part 2

--- 2024/src/Day12.hs
+++ 2024/src/Day12.hs
@@ -1,29 +1,45 @@
 module Day12 (part1, part2) where
 
-import Control.Arrow (Arrow (second))
-import Data.Bifunctor (bimap)
+import Control.Arrow (Arrow (second, (&&&)))
+import Data.Set (Set)
 import Data.Set qualified as S
+import Utils (mapTuple)
 
-part1 :: String -> Int
-part1 s = sum . map (uncurry (*)) . snd $ foldl update (S.empty, []) [(r, c) | r <- [0 .. rows - 1], c <- [0 .. cols - 1]]
+neighbours :: (Int, Int) -> [(Int, Int)]
+neighbours (r, c) = [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
+
+plots :: [[Char]] -> [Set (Int, Int)]
+plots plotMap = snd $ foldl update (S.empty, []) [(r, c) | r <- [0 .. rows - 1], c <- [0 .. cols - 1]]
   where
-    plotMap = lines s
     (rows, cols) = (length plotMap, length (head plotMap))
 
-    update (visited, acc) (r, c)
-      | (r, c) `elem` visited = (visited, acc) -- TODO: delete this??
-      | otherwise = second (: acc) $ search (plotMap !! r !! c) visited (r, c)
+    update (visited, acc) (r, c) = second (: acc) $ search (plotMap !! r !! c) visited (r, c)
 
-    search expectedChar = search'
+    search expectedChar visited (r, c)
+      | outOfBounds (r, c) || (r, c) `elem` visited || plotMap !! r !! c /= expectedChar = (visited, S.empty)
+      | otherwise = foldl upd (S.insert (r, c) visited, S.singleton (r, c)) (neighbours (r, c))
       where
-        search' visited (r, c)
-          | outOfBounds (r, c) || (r, c) `elem` visited || plotMap !! r !! c /= expectedChar = (visited, (0, 0))
-          | otherwise = foldl upd (S.insert (r, c) visited, (1, 4 - length neighbors)) neighbors
-          where
-            neighbors = filter (\(r', c') -> not (outOfBounds (r', c')) && plotMap !! r' !! c' == expectedChar) [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
-            upd (vis, (a, p)) (r', c') = second (bimap (+ a) (+ p)) $ search' vis (r', c')
+        upd (vis, plot) pos = second (plot `S.union`) $ search expectedChar vis pos
 
     outOfBounds (r, c) = r < 0 || r >= rows || c < 0 || c >= cols
 
-part2 :: String -> Int
-part2 = const 2
+perimeter :: Bool -> Set (Int, Int) -> Int
+perimeter continueSides plot = sum . map (if continueSides then continuousBorders else allBorders) $ S.toList plot
+  where
+    p = (`elem` plot)
+    allBorders = (4 -) . length . filter (`elem` plot) . neighbours
+    -- Only count borders at anticlockwise end
+    continuousBorders (r, c) = length $ filter id borders
+      where
+        borders =
+          [ not (p (r + 1, c)) && not (p (r, c + 1) && not (p (r + 1, c + 1))),
+            not (p (r - 1, c)) && not (p (r, c - 1) && not (p (r - 1, c - 1))),
+            not (p (r, c + 1)) && not (p (r - 1, c) && not (p (r - 1, c + 1))),
+            not (p (r, c - 1)) && not (p (r + 1, c) && not (p (r + 1, c - 1)))
+          ]
+
+solve :: Bool -> String -> Int
+solve continousSides s = sum . map (uncurry (*) . (length &&& perimeter continousSides)) . plots $ lines s
+
+part1, part2 :: String -> Int
+(part1, part2) = mapTuple solve (False, True)
--- 2024/src/Main.hs
+++ 2024/src/Main.hs
@@ -3,11 +3,8 @@ import Day12 (part1, part2)
 main :: IO ()
 main = do
   testData <- readFile "data/day_12_test.txt"
-  testData2 <- readFile "data/day_12_test_2.txt"
   realData <- readFile "data/day_12.txt"
   print $ part1 testData
-  print $ part1 testData2
   print $ part1 realData
-  -- print $ part2 testData
-  -- print $ part2 realData
-  return ()
+  print $ part2 testData
+  print $ part2 realData
