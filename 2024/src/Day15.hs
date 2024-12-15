fe03e18c5b1b0bcf1e994e3a2c60fe08f966e858
thomasschafer <thomasschafer97@gmail.com>
2024-12-15 21:46:08 +0000

    Add day 15 part 2

--- 2024/src/Day15.hs
+++ 2024/src/Day15.hs
@@ -1,42 +1,25 @@
 module Day15 (part1, part2) where
 
-import Control.Arrow (Arrow (first, second))
+import Control.Arrow (Arrow (first))
+import Control.Monad ((<=<))
 import Data.Bifunctor (bimap)
 import Data.Foldable (find)
-import Data.List (elemIndex)
 import Data.List.Split (splitOn)
-import Data.Maybe (mapMaybe)
-import Utils (toTuple)
+import Data.Maybe (fromMaybe, mapMaybe)
+import Utils (mapTuple, toTuple)
 
-data Obj = Robot | Box | Wall deriving (Eq, Show)
+data Obj = Robot | Box | BoxLeft | BoxRight | Wall deriving (Eq, Show)
 
 type Grid = [[Maybe Obj]]
 
-parseGrid :: [String] -> Grid
-parseGrid = map . map $ \case
-  '#' -> Just Wall
-  'O' -> Just Box
-  '@' -> Just Robot
-  '.' -> Nothing
-  c -> error $ "Unexpected grid char " ++ show c
+data Move = U | D | L | R deriving (Eq, Show)
 
-data Move = U | D | L | R deriving (Show)
-
-parseMoves :: String -> [Move]
-parseMoves = mapMaybe $ \case
-  '<' -> Just L
-  '>' -> Just R
-  '^' -> Just U
-  'v' -> Just D
-  '\n' -> Nothing
-  c -> error $ "Unexpected move char " ++ show c
-
-positionsOf :: Grid -> Maybe Obj -> [(Int, Int)]
-positionsOf grid x =
+positionsOf :: Grid -> [Maybe Obj] -> [(Int, Int)]
+positionsOf grid xs =
   [ (r, c)
     | r <- [0 .. length grid - 1],
       c <- [0 .. length (head grid) - 1],
-      grid !! r !! c == x
+      grid !! r !! c `elem` xs
   ]
 
 updateMultiple2d :: [((Int, Int), a)] -> [[a]] -> [[a]]
@@ -50,27 +33,64 @@ updateMultiple2d updates grid =
   ]
 
 applyMove :: Move -> Grid -> Grid
-applyMove move grid = case movesToFreeSq of
-  Nothing -> grid
-  Just moves -> flip updateMultiple2d grid . ((robotPos, Nothing) :) $ case move of
-    U -> [(first (subtract 1) robotPos, Just Robot), (first (subtract moves) robotPos, Just Box)]
-    D -> [(first (+ 1) robotPos, Just Robot), (first (+ moves) robotPos, Just Box)]
-    L -> [(second (subtract 1) robotPos, Just Robot), (second (subtract moves) robotPos, Just Box)]
-    R -> [(second (+ 1) robotPos, Just Robot), (second (+ moves) robotPos, Just Box)]
+applyMove move grid = case move of
+  U -> fromMaybe grid $ shiftAllVertical (-1) robotPos grid
+  D -> fromMaybe grid $ shiftAllVertical 1 robotPos grid
+  L -> fromMaybe grid $ shiftAllHorizontal (-1) robotPos grid
+  R -> fromMaybe grid $ shiftAllHorizontal 1 robotPos grid
   where
-    robotPos@(robotRow, robotCol) = head $ positionsOf grid (Just Robot)
-    view = takeWhile (/= Just Wall) $ case move of
-      U -> reverse $ map (!! robotCol) $ take robotRow grid
-      D -> map (!! robotCol) $ drop (robotRow + 1) grid
-      L -> reverse $ take robotCol (grid !! robotRow)
-      R -> drop (robotCol + 1) (grid !! robotRow)
-    movesToFreeSq = (+ 1) <$> elemIndex Nothing view
+    robotPos = head $ positionsOf grid [Just Robot]
+
+shiftAllVertical :: Int -> (Int, Int) -> Grid -> Maybe Grid
+shiftAllVertical step (r, c) grid = case grid !! (r + step) !! c of
+  Nothing -> Just $ moveCur grid
+  Just Wall -> Nothing
+  Just Box -> moveCur <$> shiftAllVertical step (r + step, c) grid
+  Just BoxLeft -> moveCur <$> (shiftAllVertical step (r + step, c + 1) <=< shiftAllVertical step (r + step, c)) grid
+  Just BoxRight -> moveCur <$> (shiftAllVertical step (r + step, c - 1) <=< shiftAllVertical step (r + step, c)) grid
+  where
+    moveCur = updateMultiple2d [((r, c), Nothing), ((r + step, c), grid !! r !! c)]
+
+shiftAllHorizontal :: Int -> (Int, Int) -> Grid -> Maybe Grid
+shiftAllHorizontal step (r, c) grid = case grid !! r !! (c + step) of
+  Nothing -> Just $ moveCur grid
+  Just Wall -> Nothing
+  Just b | b `elem` [Box, BoxLeft, BoxRight] -> moveCur <$> shiftAllHorizontal step (r, c + step) grid
+  where
+    moveCur = updateMultiple2d [((r, c), Nothing), ((r, c + step), grid !! r !! c)]
 
 gpsCoords :: Grid -> Int
-gpsCoords grid = sum . map (uncurry (+) . first (100 *)) $ positionsOf grid (Just Box)
+gpsCoords grid = sum . map (uncurry (+) . first (100 *)) . positionsOf grid $ map Just [Box, BoxLeft]
 
-part1 :: String -> Int
-part1 = gpsCoords . uncurry (foldl $ flip applyMove) . bimap (parseGrid . lines) parseMoves . toTuple . splitOn "\n\n"
+parse :: String -> (Grid, [Move])
+parse = bimap (parseGrid . lines) parseMoves . toTuple . splitOn "\n\n"
+  where
+    parseGrid = map . map $ \case
+      '#' -> Just Wall
+      'O' -> Just Box
+      '@' -> Just Robot
+      '.' -> Nothing
 
-part2 :: String -> Int
-part2 = const 2
+    parseMoves = mapMaybe $ \case
+      '<' -> Just L
+      '>' -> Just R
+      '^' -> Just U
+      'v' -> Just D
+      '\n' -> Nothing
+
+solve :: Bool -> String -> Int
+solve expandGrid =
+  gpsCoords
+    . uncurry (foldl $ flip applyMove)
+    . (if expandGrid then first expand else id)
+    . parse
+
+expand :: Grid -> Grid
+expand = map (concatMap expand')
+  where
+    expand' (Just Robot) = [Just Robot, Nothing]
+    expand' (Just Box) = [Just BoxLeft, Just BoxRight]
+    expand' x = [x, x]
+
+part1, part2 :: String -> Int
+(part1, part2) = mapTuple solve (False, True)
--- 2024/src/Main.hs
+++ 2024/src/Main.hs
@@ -4,9 +4,12 @@ main :: IO ()
 main = do
   testData <- readFile "data/day_15_test.txt"
   testData2 <- readFile "data/day_15_test_2.txt"
+  testData3 <- readFile "data/day_15_test_3.txt"
   realData <- readFile "data/day_15.txt"
   print $ part1 testData
   print $ part1 testData2
   print $ part1 realData
-  print $ part2 testData
+  print $ part2 testData2
+  print $ part2 testData3
   print $ part2 realData
+  return ()
