47da1ce100ce4796f7e9d15a81105403e3d01bbd
thomasschafer <thomasschafer97@gmail.com>
2024-12-17 07:14:30 +0000

    Add day 17 part 1

---
+++ 2024/src/Day17.hs
@@ -0,0 +1,58 @@
+module Day17 (part1, part2) where
+
+import Data.Bifunctor (Bifunctor (bimap))
+import Data.Bits (Bits (xor))
+import Data.List (intercalate, sort)
+import Data.List.Split (splitOn)
+import Utils (to3Tuple, toTuple)
+
+parse :: String -> ((Int, Int, Int), [Int])
+parse = bimap registers program . toTuple . splitOn "\n\n"
+  where
+    registers = to3Tuple . map (read . last . splitOn ": ") . sort . lines
+    program = map read . splitOn "," . last . splitOn ": "
+
+run :: Int -> (Int, Int, Int) -> [Int] -> [Int]
+run idx (a, b, c) program
+  | idx >= length program = []
+  | otherwise = case opcode of
+      0 ->
+        let a' = a `div` (2 ^ combo operand)
+         in run (idx + 2) (a', b, c) program
+      1 ->
+        let b' = b `xor` operand
+         in run (idx + 2) (a, b', c) program
+      2 ->
+        let b' = combo operand `mod` 8
+         in run (idx + 2) (a, b', c) program
+      3 ->
+        let ptr = (if a == 0 then idx + 2 else operand)
+         in run ptr (a, b, c) program
+      4 ->
+        let b' = b `xor` c
+         in run (idx + 2) (a, b', c) program
+      5 ->
+        let val = combo operand `mod` 8
+         in val : run (idx + 2) (a, b, c) program
+      6 ->
+        let b' = a `div` (2 ^ combo operand)
+         in run (idx + 2) (a, b', c) program
+      7 ->
+        let c' = a `div` (2 ^ combo operand)
+         in run (idx + 2) (a, b, c') program
+      _ -> error $ "Unexpected opcode " ++ show opcode
+  where
+    combo x
+      | x `elem` [0 .. 3] = x
+      | x == 4 = a
+      | x == 5 = b
+      | x == 6 = c
+      | otherwise = error $ "Invalid combo operand " ++ show x
+
+    (opcode : operand : _) = drop idx program
+
+part1 :: String -> String
+part1 = intercalate "," . map show . uncurry (run 0) . parse
+
+part2 :: String -> Int
+part2 = const 2
--- 2024/src/Main.hs
+++ 2024/src/Main.hs
@@ -1,13 +1,10 @@
-import Day16 (part1, part2)
+import Day17 (part1, part2)
 
 main :: IO ()
 main = do
-  testData <- readFile "data/day_16_test.txt"
-  testData2 <- readFile "data/day_16_test_2.txt"
-  realData <- readFile "data/day_16.txt"
+  testData <- readFile "data/day_17_test.txt"
+  realData <- readFile "data/day_17.txt"
   print $ part1 testData
-  print $ part1 testData2
   print $ part1 realData
   print $ part2 testData
-  print $ part2 testData2
   print $ part2 realData
