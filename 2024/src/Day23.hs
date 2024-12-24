fa937a5d507e62e39ee5326d3d01bd436be6bf5b
thomasschafer <thomasschafer97@gmail.com>
2024-12-24 09:44:29 +0000

    Add day 23 part 2

--- 2024/src/Day23.hs
+++ 2024/src/Day23.hs
@@ -2,9 +2,12 @@ module Day23 (part1, part2) where
 
 import Data.HashMap.Strict (HashMap)
 import Data.HashMap.Strict qualified as HM
-import Data.List (nub, sort)
+import Data.List (intercalate, maximumBy, nub, sort)
 import Data.List.Split (splitOn)
 import Data.Maybe (fromMaybe)
+import Data.Ord (comparing)
+import Data.Set (Set)
+import Data.Set qualified as S
 import Utils (toTuple)
 
 connections :: [(String, String)] -> HashMap String [String]
@@ -26,8 +29,27 @@ triangles conns = nub . concatMap (map sort . triangles') . filter ((== 't') . h
           maybe False ((neighs !! j) `elem`) $ HM.lookup (neighs !! i) conns
       ]
 
-part1 :: String -> Int
-part1 = length . triangles . connections . map (toTuple . splitOn "-") . lines
+parse :: String -> [(String, String)]
+parse = map (toTuple . splitOn "-") . lines
 
-part2 :: String -> Int
-part2 = const 2
+part1 :: String -> Int
+part1 = length . triangles . connections . parse
+
+largestClique :: HashMap String [String] -> Set String
+largestClique conns = maximumBy (comparing length) $ bronKerbosch S.empty vertices S.empty
+  where
+    vertices = S.fromList $ HM.keys conns
+
+    neighs v = maybe S.empty S.fromList $ HM.lookup v conns
+
+    bronKerbosch potential candidates excluded =
+      (if null candidates && null excluded then (potential :) else id) $
+        bronKerbosch' (S.toList candidates) candidates excluded
+      where
+        bronKerbosch' [] _ _ = []
+        bronKerbosch' (v : vs) cs ex =
+          bronKerbosch (S.insert v potential) (cs `S.intersection` neighs v) (ex `S.intersection` neighs v)
+            ++ bronKerbosch' vs (S.delete v cs) (S.insert v ex)
+
+part2 :: String -> String
+part2 = intercalate "," . sort . S.toList . largestClique . connections . parse
