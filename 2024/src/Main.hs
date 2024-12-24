d9345927d42a392130c3614fe1d27ce28c8257b4
thomasschafer <thomasschafer97@gmail.com>
2024-12-24 16:59:18 +0000

    Add day 24 part 1

--- 2024/src/Main.hs
+++ 2024/src/Main.hs
@@ -1,10 +1,13 @@
-import Day23 (part1, part2)
+import Day24 (part1, part2)
 
 main :: IO ()
 main = do
-  testData <- readFile "data/day_23_test.txt"
-  realData <- readFile "data/day_23.txt"
+  testData <- readFile "data/day_24_test.txt"
+  testData2 <- readFile "data/day_24_test_2.txt"
+  realData <- readFile "data/day_24.txt"
   print $ part1 testData
+  print $ part1 testData2
   print $ part1 realData
-  print $ part2 testData
-  print $ part2 realData
+  -- print $ part2 testData
+  -- print $ part2 realData
+  return ()
