ea91ce5bc47df370733b62ceeed5266372c8d9ae
Thomas Schafer <thomasschafer97@gmail.com>
2025-12-03 06:20:56 +0000

    Add day 3 part 2

--- 2025/src/Day03.hs
+++ 2025/src/Day03.hs
@@ -2,21 +2,18 @@ module Day03 (part1, part2) where
 
 import Utils (toInt)
 
-part1 :: String -> Int
-part1 = sum . map (maxJoltage . map toInt) . lines
+solve :: Int -> String -> Int
+solve n = sum . map (maxJoltage n 0 . map toInt) . lines
 
-maxJoltage :: [Int] -> Int
-maxJoltage xs
-  | length xs < 2 = 0
-  | otherwise =
-      maximum
-        [ (head xs) * 10 + last xs,
-          maxShiftLeft,
-          maxShiftRight
-        ]
+maxJoltage :: Int -> Int -> [Int] -> Int
+maxJoltage 0 acc _ = acc
+maxJoltage n acc xs = maxJoltage (n - 1) (10 * acc + firstDigit) rest
   where
-    maxShiftLeft = maxJoltage . dropWhile (<= head xs) $ tail xs
-    maxShiftRight = maxJoltage . reverse . dropWhile (<= last xs) . reverse $ init xs
+    firstDigit = maximum $ take (length xs - (n - 1)) xs
+    rest = tail $ dropWhile (< firstDigit) xs
+
+part1 :: String -> Int
+part1 = solve 2
 
 part2 :: String -> Int
-part2 = const 2
+part2 = solve 12
