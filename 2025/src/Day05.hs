8a9dd58cd10d02e8e4a8d58cf5aeea0568d12b92
Thomas Schafer <thomasschafer97@gmail.com>
2025-12-05 06:21:03 +0000

    Add day 5 part 2

--- 2025/src/Day05.hs
+++ 2025/src/Day05.hs
@@ -1,19 +1,26 @@
 module Day05 (part1, part2) where
 
 import Data.Bifunctor (bimap)
+import Data.List (sort)
 import Data.List.Split (splitOn)
 import Utils (mapTuple, toTuple, (...))
 
-part1 :: String -> Int
-part1 = uncurry freshIngredients . bimap (map parseRange) (map read) . mapTuple lines . toTuple . splitOn "\n\n"
+parse :: String -> ([(Int, Int)], [Int])
+parse = bimap (map parseRange) (map read) . mapTuple lines . toTuple . splitOn "\n\n"
   where
     parseRange = toTuple . map read . splitOn "-"
 
-freshIngredients :: [(Int, Int)] -> [Int] -> Int
-freshIngredients = length ... filter . (flip $ any . inRange)
+part1 :: String -> Int
+part1 = uncurry freshIngredients . parse
+  where
+    freshIngredients = length ... filter . (flip $ any . inRange)
+    inRange n (lower, upper) = n >= lower && n <= upper
 
-inRange :: Int -> (Int, Int) -> Bool
-inRange n (lower, upper) = n >= lower && n <= upper
+rangeOverlap :: [(Int, Int)] -> Int
+rangeOverlap ((l1, u1) : (l2, u2) : rest)
+  | u1 >= l2 = rangeOverlap ((l1, max u1 u2) : rest)
+rangeOverlap ((lower, upper) : rest) = upper - lower + 1 + rangeOverlap rest
+rangeOverlap [] = 0
 
 part2 :: String -> Int
-part2 = const 2
+part2 = rangeOverlap . sort . fst . parse
