fd0681ab2aa5b8c6ace7a6731fe29b9aec56fac4
Thomas Schafer <thomasschafer97@gmail.com>
2025-12-10 07:18:55 +0000

    WIP day 10 part 2

--- 2025/src/Day10.hs
+++ 2025/src/Day10.hs
@@ -2,21 +2,22 @@ module Day10 (part1, part2) where
 
 import Data.Bits (Bits (xor))
 import Data.List.Split (splitOn)
-import Data.Set (Set)
 import Data.Set qualified as S
 
-parse :: String -> (Int, [Int], [Int])
+parse :: String -> ([Bool], [[Int]], [Int])
 parse s = (lights, wiring, joltage)
   where
     [l, w, j] = concatMap (splitOn ") {") $ splitOn "] (" s
-    lights = sum . zipWith (*) [2 ^ i | i <- [0 ..]] . map (fromEnum . (== '#')) $ tail l
-    wiring = map (sum . map ((2 ^) . read) . splitOn ",") $ splitOn ") (" w
+    lights = map (== '#') $ tail l
+    wiring = map (map (read) . splitOn ",") $ splitOn ") (" w
     joltage = map read . splitOn "," $ init j
 
-fewestPresses :: (Int, [Int], [Int]) -> Int
-fewestPresses (desired, switches, _) = go (S.singleton 0) 0
+fewestPressesIndicators :: [Bool] -> [[Int]] -> Int
+fewestPressesIndicators desiredBools switchesList = go (S.singleton 0) 0
   where
-    go :: Set Int -> Int -> Int
+    desired = sum . zipWith (*) [2 ^ i | i <- [0 ..]] $ map fromEnum desiredBools
+    switches = map (sum . map (2 ^)) switchesList
+
     go cur count
       | desired `S.member` cur = count
       | otherwise = go new (count + 1)
@@ -24,7 +25,20 @@ fewestPresses (desired, switches, _) = go (S.singleton 0) 0
         new = foldl (flip S.insert) cur $ [s `xor` c | s <- switches, c <- S.toList cur]
 
 part1 :: String -> Int
-part1 = sum . map (fewestPresses . parse) . lines
+part1 = sum . map (uncurry fewestPressesIndicators . (\(l, w, _) -> (l, w)) . parse) . lines
+
+fewestPressesJoltage :: [[Int]] -> [Int] -> Int
+fewestPressesJoltage switchesLists desired = go (S.singleton $ map (const 0) desired) 0
+  where
+    switches =
+      map (\s -> map fromEnum [i `elem` s | i <- [0 .. length desired - 1]]) $
+        map S.fromList switchesLists
+
+    go cur count
+      | desired `S.member` cur = count
+      | otherwise = go new (count + 1)
+      where
+        new = foldl (flip S.insert) cur $ [res | s <- switches, c <- S.toList cur, let res = zipWith (+) s c, and $ zipWith (<=) res desired]
 
 part2 :: String -> Int
-part2 = const 2
+part2 = sum . map (uncurry fewestPressesJoltage . (\(_, w, j) -> (w, j)) . parse) . lines
