4d1f7c7702996daa25d439538eda9dde947dcc64
Thomas Schafer <thomasschafer97@gmail.com>
2025-12-11 06:39:21 +0000

    Add day 11 part 2

--- 2025/src/Day11.hs
+++ 2025/src/Day11.hs
@@ -9,25 +9,30 @@ import Utils (toTuple, (...))
 
 data Status = InProgress | Visited {numPaths :: Int}
 
-solve :: HashMap String [String] -> Int
-solve connections = fst $ go HM.empty "you"
+countPaths :: String -> String -> HashMap String [String] -> Int
+countPaths start end connections = fst $ go HM.empty start
   where
-    go visited "out" = (1, visited)
-    go visited cur = case HM.lookup cur visited of
-      Just InProgress -> (0, visited)
-      Just (Visited {numPaths}) -> (numPaths, visited)
-      Nothing -> (n, HM.insert cur (Visited {numPaths = n}) newVisited)
-        where
-          (n, newVisited) =
-            foldl (uncurry update) (0, HM.insert cur InProgress visited)
-              . fromMaybe []
-              $ HM.lookup cur connections
-          update count = first (count +) ... go
+    go visited cur
+      | cur == end = (1, visited)
+      | otherwise = case HM.lookup cur visited of
+          Just InProgress -> (0, visited)
+          Just (Visited {numPaths}) -> (numPaths, visited)
+          Nothing -> (n, HM.insert cur (Visited {numPaths = n}) newVisited)
+            where
+              (n, newVisited) =
+                foldl (uncurry update) (0, HM.insert cur InProgress visited)
+                  . fromMaybe []
+                  $ HM.lookup cur connections
+              update count = first (count +) ... go
+
+parse :: String -> HashMap String [String]
+parse = HM.fromList . map (second words . toTuple . splitOn ": ") . lines
 
 part1 :: String -> Int
-part1 = solve . HM.fromList . map parse . lines
-  where
-    parse = second words . toTuple . splitOn ": "
+part1 = countPaths "you" "out" . parse
 
 part2 :: String -> Int
-part2 = const 2
+part2 s = (paths "svr" "dac" * paths "dac" "fft" * paths "fft" "out") + (paths "svr" "fft" * paths "fft" "dac" * paths "dac" "out")
+  where
+    connections = parse s
+    paths start end = countPaths start end connections
